a761263805dcbcc55676a1e6c653259a6dc3b054
Robert Sehr <Robert.Sehr@intranda.com>
2013-01-24 09:16:10 +0100

    fixed error message

--- goobi1.9/WEB-INF/src/org/goobi/production/flow/helper/JobCreation.java
+++ goobi1.9/WEB-INF/src/org/goobi/production/flow/helper/JobCreation.java
@@ -61,7 +61,7 @@ public class JobCreation {
 		File metsfile = new File(metsfilename);
 		Prozess p = null;
 		if (!testTitle(processTitle)) {
-			logger.error("cannot create process, process title " + processTitle + "is allready in use");
+			logger.error("cannot create process, process title \"" + processTitle + "\" is already in use");
 			// removing all data
 			File imagesFolder = new File(basepath);
 			if (imagesFolder.exists() && imagesFolder.isDirectory()) {
