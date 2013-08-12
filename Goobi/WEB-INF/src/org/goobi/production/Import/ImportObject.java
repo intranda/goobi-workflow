f092a100e05f10afb3fbf2cc5edd18b0e2be26c4
Robert Sehr <robert.sehr@intranda.com>
2013-08-12 13:48:33 +0200

    do not remove import files if import fails

--- Goobi/WEB-INF/src/org/goobi/production/Import/ImportObject.java
+++ Goobi/WEB-INF/src/org/goobi/production/Import/ImportObject.java
@@ -1,4 +1,5 @@
 package org.goobi.production.Import;
+
 /**
  * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
  * 
@@ -37,80 +38,97 @@ import de.sub.goobi.Beans.Werkstueckeigenschaft;
 
 public class ImportObject {
 
-	// TODO must end with ".xml" in current implementation
-	private String processTitle ="";
-	private Integer batchId;
-	
-	// TODO needed?
-	private String metsFilename ="";
+    // TODO must end with ".xml" in current implementation
+    private String processTitle = "";
+    private Integer batchId;
 
-	// error handling
-	private ImportReturnValue importReturnValue = ImportReturnValue.ExportFinished;
-	private String errorMessage ="";
-	
-	// additional information
-	private List<Prozesseigenschaft> processProperties = new ArrayList<Prozesseigenschaft>();
-	private List<Werkstueckeigenschaft> workProperties = new ArrayList<Werkstueckeigenschaft>();
-	private List<Vorlageeigenschaft> templateProperties = new ArrayList<Vorlageeigenschaft>();
-	
-	
-	
-	public ImportObject() {
-		
-	
-	}
-	
-	
-	
-	
-	public String getProcessTitle() {
-		return this.processTitle;
-	}
-	public void setProcessTitle(String processTitle) {
-		this.processTitle = processTitle;
-	}
-	public String getMetsFilename() {
-		return this.metsFilename;
-	}
-	public void setMetsFilename(String metsFilename) {
-		this.metsFilename = metsFilename;
-	}
-	public ImportReturnValue getImportReturnValue() {
-		return this.importReturnValue;
-	}
-	public void setImportReturnValue(ImportReturnValue importReturnValue) {
-		this.importReturnValue = importReturnValue;
-	}
-	public String getErrorMessage() {
-		return this.errorMessage;
-	}
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
-	public List<Prozesseigenschaft> getProcessProperties() {
-		return this.processProperties;
-	}
-	public void setProcessProperties(List<Prozesseigenschaft> processProperties) {
-		this.processProperties = processProperties;
-	}
-	public List<Werkstueckeigenschaft> getWorkProperties() {
-		return this.workProperties;
-	}
-	public void setWorkProperties(List<Werkstueckeigenschaft> workProperties) {
-		this.workProperties = workProperties;
-	}
-	public List<Vorlageeigenschaft> getTemplateProperties() {
-		return this.templateProperties;
-	}
-	public void setTemplateProperties(List<Vorlageeigenschaft> templateProperties) {
-		this.templateProperties = templateProperties;
-	}
-	public Integer getBatchId() {
-		return batchId;
-	}
-	public void setBatchId(Integer batchId) {
-		this.batchId = batchId;
-	}
-	
-	
+    private String metsFilename = "";
+
+    private String importFileName = "";
+
+    // error handling
+    private ImportReturnValue importReturnValue = ImportReturnValue.ExportFinished;
+    private String errorMessage = "";
+
+    // additional information
+    private List<Prozesseigenschaft> processProperties = new ArrayList<Prozesseigenschaft>();
+    private List<Werkstueckeigenschaft> workProperties = new ArrayList<Werkstueckeigenschaft>();
+    private List<Vorlageeigenschaft> templateProperties = new ArrayList<Vorlageeigenschaft>();
+
+    public ImportObject() {
+
+    }
+
+    public String getProcessTitle() {
+        return this.processTitle;
+    }
+
+    public void setProcessTitle(String processTitle) {
+        this.processTitle = processTitle;
+    }
+
+    public String getMetsFilename() {
+        return this.metsFilename;
+    }
+
+    public void setMetsFilename(String metsFilename) {
+        this.metsFilename = metsFilename;
+    }
+
+    public ImportReturnValue getImportReturnValue() {
+        return this.importReturnValue;
+    }
+
+    public void setImportReturnValue(ImportReturnValue importReturnValue) {
+        this.importReturnValue = importReturnValue;
+    }
+
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public List<Prozesseigenschaft> getProcessProperties() {
+        return this.processProperties;
+    }
+
+    public void setProcessProperties(List<Prozesseigenschaft> processProperties) {
+        this.processProperties = processProperties;
+    }
+
+    public List<Werkstueckeigenschaft> getWorkProperties() {
+        return this.workProperties;
+    }
+
+    public void setWorkProperties(List<Werkstueckeigenschaft> workProperties) {
+        this.workProperties = workProperties;
+    }
+
+    public List<Vorlageeigenschaft> getTemplateProperties() {
+        return this.templateProperties;
+    }
+
+    public void setTemplateProperties(List<Vorlageeigenschaft> templateProperties) {
+        this.templateProperties = templateProperties;
+    }
+
+    public Integer getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(Integer batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getImportFileName() {
+        return importFileName;
+    }
+
+    public void setImportFileName(String importFileName) {
+        this.importFileName = importFileName;
+    }
+
 }
