22155d9b8a6c04cede2206be76c69f0e2689dec2
Robert Sehr <robert.sehr@intranda.com>
2013-11-15 15:42:10 +0100

    added test suite for all tests

---
+++ Goobi/test/src/de/sub/goobi/TestAll.java
@@ -0,0 +1,14 @@
+package de.sub.goobi;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({de.sub.goobi.config.TestAll.class, de.sub.goobi.converter.TestAll.class})
+public class TestAll {
+
+  
+
+}
