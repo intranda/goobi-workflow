177845ca3597df1be8978c17df91e784f48071d6
Robert Sehr <robert.sehr@intranda.com>
2014-01-06 09:35:44 +0100

    fixed possible nullpointer exception when updating current session

--- Goobi/src/de/sub/goobi/helper/servletfilter/SessionCounterFilter.java
+++ Goobi/src/de/sub/goobi/helper/servletfilter/SessionCounterFilter.java
@@ -1,4 +1,5 @@
 package de.sub.goobi.helper.servletfilter;
+
 /**
  * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
  * 
@@ -31,9 +32,11 @@ package de.sub.goobi.helper.servletfilter;
 import java.io.IOException;
 
 import javax.faces.FactoryFinder;
+import javax.faces.application.Application;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 import javax.faces.context.FacesContextFactory;
+import javax.faces.el.ValueBinding;
 import javax.faces.lifecycle.Lifecycle;
 import javax.faces.lifecycle.LifecycleFactory;
 import javax.servlet.Filter;
@@ -47,79 +50,71 @@ import javax.servlet.http.HttpServletRequest;
 
 import de.sub.goobi.forms.SessionForm;
 
-
 public class SessionCounterFilter implements Filter {
-   ServletContext servletContext;
+    ServletContext servletContext;
 
-   public void init(FilterConfig filterConfig) throws ServletException {
-      servletContext = filterConfig.getServletContext();
-   }
+    public void init(FilterConfig filterConfig) throws ServletException {
+        servletContext = filterConfig.getServletContext();
+    }
 
-   
-
-   /**
+    /**
     */
-   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-         throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+        HttpServletRequest httpReq = (HttpServletRequest) request;
+        FacesContext context = getFacesContext(request, response);
+        Application application = context.getApplication();
+        if (application != null) {
+            ValueBinding binding = application.createValueBinding("#{SessionForm}");
+            if (binding != null && context != null) {
 
-      HttpServletRequest httpReq = (HttpServletRequest) request;
-      FacesContext context = getFacesContext(request, response);
-      SessionForm sf = (SessionForm) context.getApplication().createValueBinding("#{SessionForm}").getValue(
-            context);
-      sf.sessionAktualisieren(httpReq.getSession());
+                SessionForm sf = (SessionForm) binding.getValue(context);
+                sf.sessionAktualisieren(httpReq.getSession());
+            }
+        }
+        chain.doFilter(request, response);
+    }
 
-      chain.doFilter(request, response);
-   }
+    // You need an inner class to be able to call FacesContext.setCurrentInstance
+    // since it's a protected method
+    private abstract static class InnerFacesContext extends FacesContext {
+        protected static void setFacesContextAsCurrentInstance(FacesContext facesContext) {
+            FacesContext.setCurrentInstance(facesContext);
+        }
+    }
 
-   
+    private FacesContext getFacesContext(ServletRequest request, ServletResponse response) {
+        // Try to get it first
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        if (facesContext != null)
+            return facesContext;
 
-   // You need an inner class to be able to call FacesContext.setCurrentInstance
-   // since it's a protected method
-   private abstract static class InnerFacesContext extends FacesContext {
-      protected static void setFacesContextAsCurrentInstance(FacesContext facesContext) {
-         FacesContext.setCurrentInstance(facesContext);
-      }
-   }
+        FacesContextFactory contextFactory = (FacesContextFactory) FactoryFinder.getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
+        LifecycleFactory lifecycleFactory = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
+        Lifecycle lifecycle = lifecycleFactory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
 
-   
+        // Either set a private member servletContext = filterConfig.getServletContext();
+        // in you filter init() method or set it here like this:
+        // ServletContext servletContext = ((HttpServletRequest) request).getSession().getServletContext();
+        // Note that the above line would fail if you are using any other protocol than http
 
-   private FacesContext getFacesContext(ServletRequest request, ServletResponse response) {
-      // Try to get it first
-      FacesContext facesContext = FacesContext.getCurrentInstance();
-      if (facesContext != null)
-         return facesContext;
+        // Doesn't set this instance as the current instance of FacesContext.getCurrentInstance
+        facesContext = contextFactory.getFacesContext(servletContext, request, response, lifecycle);
 
-      FacesContextFactory contextFactory = (FacesContextFactory) FactoryFinder
-            .getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
-      LifecycleFactory lifecycleFactory = (LifecycleFactory) FactoryFinder
-            .getFactory(FactoryFinder.LIFECYCLE_FACTORY);
-      Lifecycle lifecycle = lifecycleFactory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
+        // Set using our inner class
+        InnerFacesContext.setFacesContextAsCurrentInstance(facesContext);
 
-      // Either set a private member servletContext = filterConfig.getServletContext();
-      // in you filter init() method or set it here like this:
-      // ServletContext servletContext = ((HttpServletRequest) request).getSession().getServletContext();
-      // Note that the above line would fail if you are using any other protocol than http
+        // set a new viewRoot, otherwise context.getViewRoot returns null
+        UIViewRoot view = facesContext.getApplication().getViewHandler().createView(facesContext, "yourOwnID");
+        facesContext.setViewRoot(view);
 
-      // Doesn't set this instance as the current instance of FacesContext.getCurrentInstance
-      facesContext = contextFactory.getFacesContext(servletContext, request, response, lifecycle);
+        return facesContext;
+    }
 
-      // Set using our inner class
-      InnerFacesContext.setFacesContextAsCurrentInstance(facesContext);
-
-      // set a new viewRoot, otherwise context.getViewRoot returns null
-      UIViewRoot view = facesContext.getApplication().getViewHandler().createView(facesContext, "yourOwnID");
-      facesContext.setViewRoot(view);
-
-      return facesContext;
-   }
-
-   
-
-   /**
+    /**
     */
-   public void destroy() {
-      // Nothing necessary
-   }
+    public void destroy() {
+        // Nothing necessary
+    }
 
 }
