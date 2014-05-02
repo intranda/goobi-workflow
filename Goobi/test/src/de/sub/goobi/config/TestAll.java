cd3e1329d0a6a873b0e713b196550f94c1adc99b
Robert Sehr <robert.sehr@intranda.com>
2014-05-02 13:18:33 +0200

    added new test to suite

--- Goobi/test/src/de/sub/goobi/config/TestAll.java
+++ Goobi/test/src/de/sub/goobi/config/TestAll.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({ ConfigProjectsTest.class, DigitalCollectionsTest.class})
+@SuiteClasses({ ConfigProjectsTest.class, DigitalCollectionsTest.class, ConfigurationHelperTest.class})
 public class TestAll {
 
   
