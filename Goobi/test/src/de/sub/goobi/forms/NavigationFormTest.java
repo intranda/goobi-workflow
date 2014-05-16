f188b8068152eeee3833ad8c7724ed192b8ce85c
Robert Sehr <robert.sehr@intranda.com>
2014-05-16 14:31:52 +0200

    added test class for navigation bean

---
+++ Goobi/test/src/de/sub/goobi/forms/NavigationFormTest.java
@@ -0,0 +1,15 @@
+package de.sub.goobi.forms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NavigationFormTest {
+
+    @Test
+    public void testConstructor() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+    }
+
+}
--- Goobi/test/src/de/sub/goobi/forms/TestAll.java
+++ Goobi/test/src/de/sub/goobi/forms/TestAll.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({AdditionalFieldTest.class, AdministrationFormTest.class, MassImportFormTest.class, HelperFormTest.class, LongRunningTasksForm.class})
+@SuiteClasses({AdditionalFieldTest.class, AdministrationFormTest.class, MassImportFormTest.class, HelperFormTest.class, LongRunningTasksFormTest.class, NavigationFormTest.class})
 public class TestAll {
 
 }
