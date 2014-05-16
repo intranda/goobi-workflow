4910d928ca0f62d912088b2c1da07adef5b2ada7
Robert Sehr <robert.sehr@intranda.com>
2014-05-16 14:44:27 +0200

    added tests for NavigationForm

--- Goobi/test/src/de/sub/goobi/forms/NavigationFormTest.java
+++ Goobi/test/src/de/sub/goobi/forms/NavigationFormTest.java
@@ -12,4 +12,86 @@ public class NavigationFormTest {
         assertNotNull(form);
     }
 
+    @Test
+    public void testAktuell() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        String fixture = "test";
+        form.setAktuell(fixture);
+        assertEquals(fixture, form.getAktuell());
+    }
+
+    @Test
+    public void testReload() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertEquals("", form.Reload());
+    }
+
+    @Test
+    public void testJeniaPopupCloseAction() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertEquals("jeniaClosePopupFrameWithAction", form.JeniaPopupCloseAction());
+    }
+
+    @Test
+    public void testBenutzerBearbeiten() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertEquals("BenutzerBearbeiten", form.BenutzerBearbeiten());
+    }
+
+    @Test
+    public void testGetShowTaskManager() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertFalse(form.getShowTaskManager());
+    }
+
+    @Test
+    public void testGetShowModuleManager() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertFalse(form.getShowModuleManager());
+    }
+
+    @Test
+    public void testIsShowHelp() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertFalse(form.isShowHelp());
+        form.setShowHelp(true);
+        assertTrue(form.isShowHelp());
+    }
+
+    @Test
+    public void testIsShowExpertView() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertFalse(form.isShowExpertView());
+        form.setShowExpertView(true);
+        assertTrue(form.isShowExpertView());
+    }
+
+    @Test
+    public void testShowSidebar() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertTrue(form.isShowSidebar());
+        form.setShowSidebar(false);
+        assertFalse(form.isShowSidebar());
+    }
+
+    @Test
+    public void testActiveTab() {
+        NavigationForm form = new NavigationForm();
+        assertNotNull(form);
+        assertEquals("productionStatistics", form.getActiveTab());
+
+        String fixture = "test";
+        form.setActiveTab(fixture);
+        assertEquals(fixture, form.getActiveTab());
+    }
+
 }
