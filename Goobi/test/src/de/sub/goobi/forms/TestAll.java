3338db4ca6855e1d6abbfefc4723f94ee57823ea
Robert Sehr <robert.sehr@intranda.com>
2014-05-23 15:40:55 +0200

    enabled SpracheFormTest

--- Goobi/test/src/de/sub/goobi/forms/TestAll.java
+++ Goobi/test/src/de/sub/goobi/forms/TestAll.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AdditionalFieldTest.class, AdministrationFormTest.class, MassImportFormTest.class, HelperFormTest.class,
-        LongRunningTasksFormTest.class, NavigationFormTest.class, SessionFormTest.class, ProzesskopieFormTest.class })
+        LongRunningTasksFormTest.class, NavigationFormTest.class, SessionFormTest.class, ProzesskopieFormTest.class, SpracheFormTest.class })
 public class TestAll {
 
 }
