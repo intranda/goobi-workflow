2026429e2cd7d9d1759e83f73b72b7d4c872893a
Robert Sehr <robert.sehr@intranda.com>
2014-07-17 14:30:59 +0200

    added toLowerCase to ats

--- Goobi/src/de/sub/goobi/forms/ProzesskopieForm.java
+++ Goobi/src/de/sub/goobi/forms/ProzesskopieForm.java
@@ -1574,7 +1574,7 @@ public class ProzesskopieForm {
                 wordNo++;
             }
         }
-        String res = ughHelper.convertUmlaut(result.toString());
+        String res = ughHelper.convertUmlaut(result.toString()).toLowerCase();
         return res.replaceAll("[\\W]", ""); // delete umlauts etc.
     }
 
