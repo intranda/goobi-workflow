7a6e514108a22a61f57bea2eb962279d0fda796d
Robert Sehr <robert.sehr@intranda.com>
2015-12-03 11:48:59 +0100

    added validator to check, if selected time is between 10 and 30 minutes

---
+++ Goobi/src/org/goobi/validator/TimeValidator.java
@@ -0,0 +1,29 @@
+package org.goobi.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import de.sub.goobi.helper.Helper;
+
+@FacesValidator("org.goobi.validator.TimeValidator")
+public class TimeValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String data = value.toString();      
+        int time = new Integer(data).intValue();
+        if (time != 0) {
+            if (time < 10 || time > 30) {
+                FacesMessage msg = new FacesMessage(Helper.getTranslation("metadataSaveTimeError"), Helper.getTranslation("metadataSaveTimeError"));
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+                throw new ValidatorException(msg);
+            }
+        }
+
+    }
+
+}
