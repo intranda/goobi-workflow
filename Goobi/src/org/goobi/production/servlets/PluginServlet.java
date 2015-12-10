c4b0670945bd452285364a8602b0cca04bc41507
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2015-12-10 08:36:52 +0100

    added new servlet plugin api

---
+++ Goobi/src/org/goobi/production/servlets/PluginServlet.java
@@ -0,0 +1,48 @@
+package org.goobi.production.servlets;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.goobi.production.enums.PluginType;
+import org.goobi.production.plugin.PluginLoader;
+import org.goobi.production.plugin.interfaces.IServletPlugin;
+
+/**
+ * Servlet implementation class PluginServlet
+ */
+public class PluginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PluginServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Map<String, String[]> parameter = request.getParameterMap();
+		if (parameter.get("action") != null) {
+			String action = parameter.get("action")[0];
+			IServletPlugin servlet = (IServletPlugin) PluginLoader.getPluginByTitle(PluginType.Servlet, action);
+			servlet.execute(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ Goobi/src/org/goobi/production/servlets/ServletHelper.java
@@ -0,0 +1,61 @@
+package org.goobi.production.servlets;
+
+import javax.faces.FactoryFinder;
+import javax.faces.application.Application;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.context.FacesContextFactory;
+import javax.faces.lifecycle.Lifecycle;
+import javax.faces.lifecycle.LifecycleFactory;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ServletHelper {
+
+	private static Application getApplication(FacesContext facesContext) {
+		return facesContext.getApplication();
+	}
+
+	public static Object getManagedBean(String beanName,
+			FacesContext facesContext) {
+		return getApplication(facesContext).getVariableResolver()
+				.resolveVariable(facesContext, beanName);
+	}
+
+	public static FacesContext getFacesContext(HttpServletRequest request,
+			HttpServletResponse response) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		if (facesContext == null) {
+
+			FacesContextFactory contextFactory = (FacesContextFactory) FactoryFinder
+					.getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
+			LifecycleFactory lifecycleFactory = (LifecycleFactory) FactoryFinder
+					.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
+			Lifecycle lifecycle = lifecycleFactory
+					.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
+
+			facesContext = contextFactory.getFacesContext(request.getSession()
+					.getServletContext(), request, response, lifecycle);
+
+			// Set using our inner class
+			InnerFacesContext.setFacesContextAsCurrentInstance(facesContext);
+
+			// set a new viewRoot, otherwise context.getViewRoot returns null
+			UIViewRoot view = facesContext.getApplication().getViewHandler()
+					.createView(facesContext, "");
+			facesContext.setViewRoot(view);
+		}
+		return facesContext;
+	}
+
+	// You need an inner class to be able to call
+	// FacesContext.setCurrentInstance
+	// since it's a protected method
+	private abstract static class InnerFacesContext extends FacesContext {
+		protected static void setFacesContextAsCurrentInstance(
+				FacesContext facesContext) {
+			FacesContext.setCurrentInstance(facesContext);
+		}
+	}
+}
