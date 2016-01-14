65875f372b0686774dc09afa849b4e71bdcc83d8
Robert Sehr <robert.sehr@intranda.com>
2016-01-14 13:22:36 +0100

    added gpl header

--- Goobi/src/de/sub/goobi/metadaten/Image.java
+++ Goobi/src/de/sub/goobi/metadaten/Image.java
@@ -1,13 +1,33 @@
 package de.sub.goobi.metadaten;
 
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2015 Robert Sehr <robert.sehr@intranda.com>
+ *
+ * All rights reserved
+ *
+ * This file is part of the Goobi project. The Goobi project is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * This copyright notice MUST APPEAR in all copies of this file!
+ ***************************************************************/
+
 import java.awt.Dimension;
-import java.io.IOException;
-import java.net.URISyntaxException;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import de.unigoettingen.sub.commons.contentlib.exceptions.ContentLibImageException;
-import de.unigoettingen.sub.commons.contentlib.servlet.controller.GetImageDimensionAction;
 import lombok.Data;
 
 public @Data class Image {
