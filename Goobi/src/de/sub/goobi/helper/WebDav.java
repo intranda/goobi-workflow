68d7f85314a01b438b67d6bb23d56bc5e0f59e3e
Oliver Paetzel <oliver.paetzel@intranda.com>
2018-05-17 14:40:46 +0200

    fix issues with tifWriter.conf and linking home user in s3

--- Goobi/src/de/sub/goobi/helper/WebDav.java
+++ Goobi/src/de/sub/goobi/helper/WebDav.java
@@ -28,7 +28,8 @@ package de.sub.goobi.helper;
  * exception statement from your version.
  */
 import java.io.BufferedWriter;
-import java.io.FileOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Serializable;
@@ -39,8 +40,8 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.goobi.beans.User;
 import org.goobi.beans.Process;
+import org.goobi.beans.User;
 import org.goobi.io.WebDavFilter;
 
 import de.sub.goobi.config.ConfigurationHelper;
@@ -48,83 +49,83 @@ import de.sub.goobi.export.download.TiffHeader;
 
 public class WebDav implements Serializable {
 
-	private static final long serialVersionUID = -1929234096626965538L;
-	private static final Logger logger = Logger.getLogger(WebDav.class);
+    private static final long serialVersionUID = -1929234096626965538L;
+    private static final Logger logger = Logger.getLogger(WebDav.class);
 
-	/*
- 	 * Kopieren bzw. symbolische Links für einen Prozess in das Benutzerhome	
-	 */
+    /*
+     * Kopieren bzw. symbolische Links für einen Prozess in das Benutzerhome	
+     */
 
-	private static String DONEDIRECTORYNAME = "fertig/";
-	public WebDav(){
-		DONEDIRECTORYNAME =ConfigurationHelper.getInstance().getDoneDirectoryName();
-	}
-	
-	
-	/**
+    private static String DONEDIRECTORYNAME = "fertig/";
+
+    public WebDav() {
+        DONEDIRECTORYNAME = ConfigurationHelper.getInstance().getDoneDirectoryName();
+    }
+
+    /**
      * Retrieve all folders from one directory ================================================================
-	 */
+     */
 
-	public List<String> UploadFromHomeAlle(String inVerzeichnis) {
-		List<String> rueckgabe = new ArrayList<String>();
-		User aktuellerBenutzer = Helper.getCurrentUser();
-		String VerzeichnisAlle;
+    public List<String> UploadFromHomeAlle(String inVerzeichnis) {
+        List<String> rueckgabe = new ArrayList<String>();
+        User aktuellerBenutzer = Helper.getCurrentUser();
+        String VerzeichnisAlle;
 
-		try {
-			VerzeichnisAlle = aktuellerBenutzer.getHomeDir() + inVerzeichnis;
-		} catch (Exception ioe) {
-			logger.error("Exception UploadFromHomeAlle()", ioe);
-			Helper.setFehlerMeldung("UploadFromHomeAlle abgebrochen, Fehler", ioe.getMessage());
-			return rueckgabe;
-		}
+        try {
+            VerzeichnisAlle = aktuellerBenutzer.getHomeDir() + inVerzeichnis;
+        } catch (Exception ioe) {
+            logger.error("Exception UploadFromHomeAlle()", ioe);
+            Helper.setFehlerMeldung("UploadFromHomeAlle abgebrochen, Fehler", ioe.getMessage());
+            return rueckgabe;
+        }
 
         List<String> dateien = StorageProvider.getInstance().list(VerzeichnisAlle, new WebDavFilter());
 
         return dateien;
 
-	}
+    }
 
-	/**
+    /**
      * Remove Folders from Directory ================================================================
-	 */
-	// TODO: Use generic types
-	public void removeFromHomeAlle(List<String> inList, String inVerzeichnis) {
-		String VerzeichnisAlle;
-		User aktuellerBenutzer = Helper.getCurrentUser();
-		try {
-			VerzeichnisAlle = aktuellerBenutzer.getHomeDir() + inVerzeichnis;
-		} catch (Exception ioe) {
-			logger.error("Exception RemoveFromHomeAlle()", ioe);
-			Helper.setFehlerMeldung("Upload stopped, error", ioe.getMessage());
-			return;
-		}
-
-		for (Iterator<String> it = inList.iterator(); it.hasNext();) {
-			String myname = it.next();
-            FilesystemHelper.deleteSymLink(VerzeichnisAlle + myname);
-		}
-	}
-
-	public void UploadFromHome(Process myProzess) {
-		User aktuellerBenutzer = Helper.getCurrentUser();
-        if (aktuellerBenutzer != null) {
-        	UploadFromHome(aktuellerBenutzer, myProzess);
+     */
+    // TODO: Use generic types
+    public void removeFromHomeAlle(List<String> inList, String inVerzeichnis) {
+        String VerzeichnisAlle;
+        User aktuellerBenutzer = Helper.getCurrentUser();
+        try {
+            VerzeichnisAlle = aktuellerBenutzer.getHomeDir() + inVerzeichnis;
+        } catch (Exception ioe) {
+            logger.error("Exception RemoveFromHomeAlle()", ioe);
+            Helper.setFehlerMeldung("Upload stopped, error", ioe.getMessage());
+            return;
         }
-	}
 
-	public void UploadFromHome(User inBenutzer, Process myProzess) {
-		String nach = "";
+        for (Iterator<String> it = inList.iterator(); it.hasNext();) {
+            String myname = it.next();
+            FilesystemHelper.deleteSymLink(VerzeichnisAlle + myname);
+        }
+    }
 
-		try {
-			nach = inBenutzer.getHomeDir();
-		} catch (Exception ioe) {
-			logger.error("Exception UploadFromHome(...)", ioe);
-			Helper.setFehlerMeldung("Aborted upload from home, error", ioe.getMessage());
-			return;
-		}
+    public void UploadFromHome(Process myProzess) {
+        User aktuellerBenutzer = Helper.getCurrentUser();
+        if (aktuellerBenutzer != null) {
+            UploadFromHome(aktuellerBenutzer, myProzess);
+        }
+    }
 
-		/* prüfen, ob Benutzer Massenupload macht */
-		if (inBenutzer != null && inBenutzer.isMitMassendownload()) {
+    public void UploadFromHome(User inBenutzer, Process myProzess) {
+        String nach = "";
+
+        try {
+            nach = inBenutzer.getHomeDir();
+        } catch (Exception ioe) {
+            logger.error("Exception UploadFromHome(...)", ioe);
+            Helper.setFehlerMeldung("Aborted upload from home, error", ioe.getMessage());
+            return;
+        }
+
+        /* prüfen, ob Benutzer Massenupload macht */
+        if (inBenutzer != null && inBenutzer.isMitMassendownload()) {
             nach += myProzess.getProjekt().getTitel() + "/";
             Path projectDirectory = Paths.get(nach = nach.replaceAll(" ", "__"));
             if (!StorageProvider.getInstance().isFileExists(projectDirectory)) {
@@ -132,124 +133,129 @@ public class WebDav implements Serializable {
                     StorageProvider.getInstance().createDirectories(projectDirectory);
                 } catch (IOException e) {
                     logger.error(e);
-				Helper.setFehlerMeldung(Helper.getTranslation("MassDownloadProjectCreationError", nach.replaceAll(" ", "__")));
-				logger.error("Can not create project directory " + nach.replaceAll(" ", "__"));
-				return;
-			}
-		}
+                    Helper.setFehlerMeldung(Helper.getTranslation("MassDownloadProjectCreationError", nach.replaceAll(" ", "__")));
+                    logger.error("Can not create project directory " + nach.replaceAll(" ", "__"));
+                    return;
+                }
+            }
         }
-		nach += myProzess.getTitel() + " [" + myProzess.getId() + "]";
+        nach += myProzess.getTitel() + " [" + myProzess.getId() + "]";
 
-		/* Leerzeichen maskieren */
-		nach = nach.replaceAll(" ", "__");
+        /* Leerzeichen maskieren */
+        nach = nach.replaceAll(" ", "__");
         Path benutzerHome = Paths.get(nach);
 
         FilesystemHelper.deleteSymLink(benutzerHome.toString());
-	}
+    }
 
-	public void DownloadToHome(Process myProzess, int inSchrittID, boolean inNurLesen) {
-		saveTiffHeader(myProzess);
-		User aktuellerBenutzer = Helper.getCurrentUser();
-		String von = "";
-		String userHome = "";
+    public void DownloadToHome(Process myProzess, int inSchrittID, boolean inNurLesen) {
+        saveTiffHeader(myProzess);
+        User aktuellerBenutzer = Helper.getCurrentUser();
+        String von = "";
+        String userHome = "";
 
-		try {
-			von = myProzess.getImagesDirectory();
-			/* UserHome ermitteln */
-			userHome = aktuellerBenutzer.getHomeDir();
+        try {
+            von = myProzess.getImagesDirectory();
+            /* UserHome ermitteln */
+            userHome = aktuellerBenutzer.getHomeDir();
 
-			/*
-			 * bei Massendownload muss auch das Projekt- und Fertig-Verzeichnis
-			 * existieren
-			 */
-			if (aktuellerBenutzer.isMitMassendownload()) {
+            /*
+             * bei Massendownload muss auch das Projekt- und Fertig-Verzeichnis
+             * existieren
+             */
+            if (aktuellerBenutzer.isMitMassendownload()) {
                 Path projekt = Paths.get(userHome + myProzess.getProjekt().getTitel());
                 FilesystemHelper.createDirectoryForUser(projekt.toString(), aktuellerBenutzer.getLogin());
-                
+
                 projekt = Paths.get(userHome + DONEDIRECTORYNAME);
                 FilesystemHelper.createDirectoryForUser(projekt.toString(), aktuellerBenutzer.getLogin());
-			}
+            }
 
-		} catch (Exception ioe) {
-			logger.error("Exception DownloadToHome()", ioe);
-			Helper.setFehlerMeldung("Aborted download to home, error", ioe.getMessage());
-			return;
-		}
+        } catch (Exception ioe) {
+            logger.error("Exception DownloadToHome()", ioe);
+            Helper.setFehlerMeldung("Aborted download to home, error", ioe.getMessage());
+            return;
+        }
 
-		/*
-		 * abhängig davon, ob der Download als "Massendownload" in einen
-		 * Projektordner erfolgen soll oder nicht, das Zielverzeichnis
-		 * definieren
-		 */
-		String processLinkName = myProzess.getTitel() + "__[" + myProzess.getId() + "]";
-		String nach = userHome;
-		if (aktuellerBenutzer.isMitMassendownload() && myProzess.getProjekt() != null) {
+        /*
+         * abhängig davon, ob der Download als "Massendownload" in einen
+         * Projektordner erfolgen soll oder nicht, das Zielverzeichnis
+         * definieren
+         */
+        String processLinkName = myProzess.getTitel() + "__[" + myProzess.getId() + "]";
+        String nach = userHome;
+        if (aktuellerBenutzer.isMitMassendownload() && myProzess.getProjekt() != null) {
             nach += myProzess.getProjekt().getTitel() + "/";
-		}
-		nach += processLinkName;
+        }
+        nach += processLinkName;
 
-		/* Leerzeichen maskieren */
-		nach = nach.replaceAll(" ", "__");
+        /* Leerzeichen maskieren */
+        nach = nach.replaceAll(" ", "__");
 
-		logger.info("von: " + von);
-		logger.info("nach: " + nach);
+        logger.info("von: " + von);
+        logger.info("nach: " + nach);
 
         Path imagePfad = Paths.get(von);
         Path benutzerHome = Paths.get(nach);
 
-		// wenn der Ziellink schon existiert, dann abbrechen
+        // wenn der Ziellink schon existiert, dann abbrechen
         if (StorageProvider.getInstance().isFileExists(benutzerHome)) {
-			return;
-		}
+            return;
+        }
 
-		String command = ConfigurationHelper.getInstance().getScriptCreateSymLink()+ " "; 
-		if (!command.isEmpty()){
-			command += imagePfad + " " + benutzerHome + " ";
-			if (inNurLesen) {
-				command += ConfigurationHelper.getInstance().getUserForImageReading();
-			} else {
-				command += aktuellerBenutzer.getLogin();
-			}
-			try {
-	            	ShellScript.legacyCallShell2(command, myProzess.getId());
-	            } catch (java.io.IOException ioe) {
-				logger.error("IOException DownloadToHome()", ioe);
-				Helper.setFehlerMeldung("Download aborted, IOException", ioe.getMessage());
-			} catch (InterruptedException e) {
-				logger.error("InterruptedException DownloadToHome()", e);
-				Helper.setFehlerMeldung("Download aborted, InterruptedException", e.getMessage());
-				logger.error(e);
-			}
-		}
-	}
+        String command = ConfigurationHelper.getInstance().getScriptCreateSymLink() + " ";
+        if (!command.isEmpty() && !ConfigurationHelper.getInstance().useS3()) {
+            command += imagePfad + " " + benutzerHome + " ";
+            if (inNurLesen) {
+                command += ConfigurationHelper.getInstance().getUserForImageReading();
+            } else {
+                command += aktuellerBenutzer.getLogin();
+            }
+            try {
+                ShellScript.legacyCallShell2(command, myProzess.getId());
+            } catch (java.io.IOException ioe) {
+                logger.error("IOException DownloadToHome()", ioe);
+                Helper.setFehlerMeldung("Download aborted, IOException", ioe.getMessage());
+            } catch (InterruptedException e) {
+                logger.error("InterruptedException DownloadToHome()", e);
+                Helper.setFehlerMeldung("Download aborted, InterruptedException", e.getMessage());
+                logger.error(e);
+            }
+        }
+    }
 
-	private void saveTiffHeader(Process inProzess) {
-		try {
-			/* prüfen, ob Tiff-Header schon existiert */
+    private void saveTiffHeader(Process inProzess) {
+        try {
+            /* prüfen, ob Tiff-Header schon existiert */
             if (StorageProvider.getInstance().isFileExists(Paths.get(inProzess.getImagesDirectory() + "tiffwriter.conf"))) {
-				return;
-			}
-			TiffHeader tif = new TiffHeader(inProzess);
-            BufferedWriter outfile =
-                    new BufferedWriter(new OutputStreamWriter(new FileOutputStream(inProzess.getImagesDirectory() + "tiffwriter.conf"), "utf-8"));
-			outfile.write(tif.getTiffAlles());
-			outfile.close();
-		} catch (Exception e) {
-			Helper.setFehlerMeldung("Download aborted", e);
-			logger.error(e);
-		}
-	}
+                return;
+            }
+            TiffHeader tif = new TiffHeader(inProzess);
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                try (BufferedWriter outwriter = new BufferedWriter(new OutputStreamWriter(baos, "utf-8"))) {
+                    outwriter.write(tif.getTiffAlles());
+                }
+                try (ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray())) {
+                    Path outPath = Paths.get(inProzess.getImagesDirectory() + "tiffwriter.conf");
+                    StorageProvider.getInstance().uploadFile(bais, outPath);
+                }
+            }
+        } catch (Exception e) {
+            Helper.setFehlerMeldung("Download aborted", e);
+            logger.error(e);
+        }
+    }
 
-	public int getAnzahlBaende(String inVerzeichnis) {
-		try {
-			User aktuellerBenutzer = Helper.getCurrentUser();
-			String VerzeichnisAlle = aktuellerBenutzer.getHomeDir() + inVerzeichnis;
+    public int getAnzahlBaende(String inVerzeichnis) {
+        try {
+            User aktuellerBenutzer = Helper.getCurrentUser();
+            String VerzeichnisAlle = aktuellerBenutzer.getHomeDir() + inVerzeichnis;
 
             return StorageProvider.getInstance().list(VerzeichnisAlle, new WebDavFilter()).size();
-		} catch (Exception e) {
-			logger.error(e);
-			return 0;
-		}
-	}
+        } catch (Exception e) {
+            logger.error(e);
+            return 0;
+        }
+    }
 
 }
