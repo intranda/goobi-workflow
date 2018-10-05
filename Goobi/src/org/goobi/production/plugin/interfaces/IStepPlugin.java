254f25c9f366bcb2e2f412f623ce6cdcb7ee50ef
Robert Sehr <robert.sehr@intranda.com>
2018-10-05 14:34:05 +0200

    added javascript to StepPlugin interface

--- Goobi/src/org/goobi/production/plugin/interfaces/IStepPlugin.java
+++ Goobi/src/org/goobi/production/plugin/interfaces/IStepPlugin.java
@@ -3,12 +3,12 @@ package org.goobi.production.plugin.interfaces;
 /**
  * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
  * 
- * Visit the websites for more information. 
+ * Visit the websites for more information.
  *     		- http://www.goobi.org
  *     		- http://launchpad.net/goobi-production
  * 		    - http://gdz.sub.uni-goettingen.de
  * 			- http://www.intranda.com
- * 			- http://digiverso.com 
+ * 			- http://digiverso.com
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option) any later version.
@@ -29,27 +29,83 @@ package org.goobi.production.plugin.interfaces;
  */
 import java.util.HashMap;
 
+import org.goobi.beans.Process;
 import org.goobi.beans.Step;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.StepReturnValue;
 
+/**
+ * This interface is used to generate plug-ins for individual steps.
+ * 
+ * @author Robert
+ *
+ */
 
 public interface IStepPlugin extends IPlugin {
 
-	public void initialize(Step step, String returnPath);
+    /**
+     * This method is used at first when the plugin is loaded.
+     * @param step the {@link Step} for which the plugin is started. step.getProzess() can be used to get additional data about {@link Process}
+     * @param returnPath the path from where the plugin was started, it is used to return to this page on cancel or finish
+     */
+    public void initialize(Step step, String returnPath);
 
-	public boolean execute();
+    /**
+     * This method is used to execute the plugin, when it was initialized. If a task is an automatic task,
+     * the method gets executed without interaction, otherwise it gets executed, when the user click on the button to open/run the plugin
+     * @return true if successful, false in error case
+     */
 
-	public String cancel();
+    public boolean execute();
 
-	public String finish();
+    /**
+     * This method can be used to cancel the edition of a plugin.
+     * 
+     * @return the path to return e.g. task_edit.xhtml or process_edit.xhtml
+     */
 
-	public HashMap<String, StepReturnValue> validate();
+    public String cancel();
 
-	public Step getStep();
+    /**
+     * In automatic tasks this method is called after execute() returned true.
+     * Otherwise it can be used to save and leave the plugin.
+     * 
+     * @return the path to return e.g. task_edit.xhtml or process_edit.xhtml
+     */
 
-	public PluginGuiType getPluginGuiType();
-	
-	public String getPagePath();
+    public String finish();
+
+    /**
+     * This method is not used by Goobi. It may be used by the plugin during initialize
+     * 
+     * @return
+     */
+
+    @Deprecated
+    public HashMap<String, StepReturnValue> validate();
+
+    /**
+     * Used to get the current step
+     * 
+     * @return step the {@link Step} for which the plugin is started
+     */
+
+    public Step getStep();
+
+    /**
+     * This method is used to define, if the plugin has no UI, a part UI or a full UI
+     * 
+     * @return the {@link PluginGuiType} of the plugin
+     */
+
+    public PluginGuiType getPluginGuiType();
+
+    /**
+     * Defines the relative URL to the starting xhtml page of the plugin. Mandatory, when the {@link PluginGuiType} is not NONE
+     * 
+     * @return path to the xhtml page
+     */
+
+    public String getPagePath();
 
 }
--- Goobi/src/org/goobi/production/plugin/interfaces/IStepPluginVersion2.java
+++ Goobi/src/org/goobi/production/plugin/interfaces/IStepPluginVersion2.java
@@ -3,12 +3,12 @@ package org.goobi.production.plugin.interfaces;
 /**
  * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
  * 
- * Visit the websites for more information. 
+ * Visit the websites for more information.
  *     		- http://www.goobi.org
  *     		- http://launchpad.net/goobi-production
  * 		    - http://gdz.sub.uni-goettingen.de
  * 			- http://www.intranda.com
- * 			- http://digiverso.com 
+ * 			- http://digiverso.com
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option) any later version.
@@ -30,10 +30,34 @@ package org.goobi.production.plugin.interfaces;
 
 import org.goobi.production.enums.PluginReturnValue;
 
+/**
+ * This interface extends the {@link IStepPlugin}
+ * 
+ * @author Robert
+ *
+ */
+
 public interface IStepPluginVersion2 extends IStepPlugin {
+
+    /**
+     * This method is be used instead of the execute() method of {@link IStepPlugin}. It allows different return values
+     * 
+     * @return {@link PluginReturnValue} the return value of the execution.
+     * <ul>
+     * <li> FINISH: the task is closed.</li>
+     * <li> WAIT: the status of the task is not changed </li>
+     * <li> ERROR: the status of the task is set to error</li>
+     * </ul>
+     * 
+     */
 
     public PluginReturnValue run();
 
+    /**
+     * The version of the current interface implementation. Currently '2' is expected.
+     * 
+     * @return the interface version
+     */
     public int getInterfaceVersion();
 
 }
