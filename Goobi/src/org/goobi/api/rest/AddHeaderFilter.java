eb53974280cfd2d5c1d6ae42c0944e0d44a0a1a8
Oliver Paetzel <oliver.paetzel@intranda.com>
2018-10-12 15:53:24 +0200

    implement CORS and custom header handling

--- Goobi/src/org/goobi/api/rest/AddHeaderFilter.java
+++ Goobi/src/org/goobi/api/rest/AddHeaderFilter.java
@@ -2,14 +2,14 @@ package org.goobi.api.rest;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
+
+import org.apache.commons.configuration.ConfigurationException;
 
 import lombok.extern.log4j.Log4j;
 
@@ -20,13 +20,27 @@ public class AddHeaderFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext req, ContainerResponseContext resp) throws IOException {
         String path = req.getUriInfo().getPath();
-        //TODO: get this from new config
-        Map<String, List<String>> headers = new TreeMap<>();
-        for (Entry<String, List<String>> headerEntry : headers.entrySet()) {
-            resp.getHeaders().addAll(headerEntry.getKey(), headerEntry.getValue());
+        if (!path.startsWith("/")) {
+            path = "/" + path;
         }
-        if (path.equals("processes/bla")) {
-            resp.getHeaders().add("Allow-Origin", "*");
+        String method = req.getMethod();
+        RestEndpointConfig rec = null;
+        try {
+            rec = RestConfig.getConfigForPath(path);
+        } catch (ConfigurationException e) {
+            log.error(e);
+        }
+        if (rec != null) {
+            if (rec.getCorsMethods().contains(method)) {
+                resp.getHeaders().addAll("Allow-Origin", rec.getCorsOrigins().toArray());
+            }
+            for (RestMethodConfig rmc : rec.getMethodConfigs()) {
+                if (rmc.getMethod().equalsIgnoreCase(method)) {
+                    for (Entry<String, List<String>> headerEntry : rmc.getCustomHeaders().entrySet()) {
+                        resp.getHeaders().addAll(headerEntry.getKey(), headerEntry.getValue().toArray());
+                    }
+                }
+            }
         }
     }
 
