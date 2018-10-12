ed49b50806bae5f9f17786fa49a2d99815f850b6
Oliver Paetzel <oliver.paetzel@intranda.com>
2018-10-12 08:27:41 +0200

    furhter work on new plugin infra

--- Goobi/src/org/goobi/production/plugin/SparkListener.java
+++ Goobi/src/org/goobi/production/plugin/SparkListener.java
@@ -4,14 +4,19 @@ import static spark.Service.ignite;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.goobi.managedbeans.LoginBean;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IPlugin;
 import org.goobi.production.plugin.interfaces.IRestGuiPlugin;
 
+import lombok.extern.log4j.Log4j;
 import spark.Service;
 import spark.route.ServletRoutes;
 import spark.servlet.SparkApplication;
 
+@Log4j
 public class SparkListener implements SparkApplication {
 
     @Override
@@ -25,6 +30,16 @@ public class SparkListener implements SparkApplication {
         final List<IPlugin> plugins = PluginLoader.getPluginList(PluginType.Step);
         ServletRoutes.get().clear();
         http.path("/plugins", () -> {
+            http.before((q, r) -> {
+                HttpServletRequest hreq = q.raw();
+                String url = hreq.getRequestURI();
+                LoginBean userBean = (LoginBean) hreq.getSession().getAttribute("LoginForm");
+                if (((userBean == null || userBean.getMyBenutzer() == null)) && !url.contains("javax.faces.resource") && !url.contains("wi?")
+                        && !url.contains("currentUsers.xhtml") && !url.contains("technicalBackground.xhtml")) {
+                    // user logged in check failed - now check for webapi token.
+
+                }
+            });
             for (IPlugin p : plugins) {
                 if (p instanceof IRestGuiPlugin) {
                     ((IRestGuiPlugin) p).initRoutes(http);
