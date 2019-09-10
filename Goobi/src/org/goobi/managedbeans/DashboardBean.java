7a73927fe8a361f9bd5f0240362e66afdd235a0b
Robert Sehr <robert.sehr@intranda.com>
2019-09-10 15:12:32 +0200

    show dashboard only when it is allowed in current institution

--- Goobi/src/org/goobi/managedbeans/DashboardBean.java
+++ Goobi/src/org/goobi/managedbeans/DashboardBean.java
@@ -2,16 +2,19 @@ package org.goobi.managedbeans;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
+import org.apache.commons.lang.StringUtils;
+import org.goobi.beans.User;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.PluginLoader;
 import org.goobi.production.plugin.interfaces.IDashboardPlugin;
 
 import de.sub.goobi.config.ConfigurationHelper;
+import de.sub.goobi.helper.Helper;
 
 @ManagedBean(name = "DashboardForm")
-@SessionScoped
+@ViewScoped
 public class DashboardBean {
 
     private IDashboardPlugin plugin = null;
@@ -22,13 +25,18 @@ public class DashboardBean {
 
     @PostConstruct
     public void initializePlugins() {
+
+        User user = Helper.getCurrentUser();
         String pluginName = ConfigurationHelper.getInstance().getDashboardPlugin();
 
-        IDashboardPlugin plugin = (IDashboardPlugin) PluginLoader.getPluginByTitle(PluginType.Dashboard, pluginName);
-        if (plugin != null) {
-            this.plugin = plugin;
-        }
+        if (StringUtils.isNotBlank(pluginName) && user != null
+                && (user.getInstitution().isAllowAllPlugins() || user.getInstitution().isDashboardPluginAllowed(pluginName))) {
 
+            IDashboardPlugin plugin = (IDashboardPlugin) PluginLoader.getPluginByTitle(PluginType.Dashboard, pluginName);
+            if (plugin != null) {
+                this.plugin = plugin;
+            }
+        }
     }
 
     public IDashboardPlugin getPlugin() {
