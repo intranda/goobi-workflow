b064b16ef9e517787db9df65cbc5925fc757c244
Oliver Paetzel <oliver.paetzel@intranda.com>
2019-09-13 11:43:46 +0200

    releaseHelper: add option to bump the public version month

--- releaseHelper.py
+++ releaseHelper.py
@@ -2,6 +2,8 @@
 
 import lxml.etree as ET
 import semver
+import sys
+import re
 
 ns={"pom": "http://maven.apache.org/POM/4.0.0"}
 
@@ -11,8 +13,23 @@ def fix_node(node):
     else:
         node.text = semver.bump_patch(node.text)+"-SNAPSHOT"
 
-def update_pom(filename, child=False):
+def update_pom(filename, child=False, bump_month=False):
     tree = ET.parse(filename)
+    if not child:
+        public_version_node = tree.find("/pom:properties/pom:publicVersion", namespaces=ns)
+        fields = re.findall(r"\d+", public_version_node.text)
+        year, month = fields[:2]
+        if bump_month:
+            if int(month) == 12:
+                year = str(int(year)+1)
+                month = "01"
+            else:
+                month = str(int(month)+1)
+            public_version_node.text = year + "." + month
+        else:
+            minor = "1" if len(fields) < 3 else int(fields[2])+1
+            public_version_node.text = year + "." + month + "-" + minor
+        
     version_node = tree.find("/pom:version", namespaces=ns)
     fix_node(version_node)
     if child:
@@ -23,7 +40,8 @@ def update_pom(filename, child=False):
     return version_node.text
 
 def main():
-    new_version = update_pom("Goobi/pom.xml")
+    bump_month = len(sys.argv) > 1 and sys.argv[1] == "--bump_month"
+    new_version = update_pom("Goobi/pom.xml", bump_month=bump_month)
     update_pom("Goobi/module-jar/pom.xml", child=True)
     update_pom("Goobi/module-war/pom.xml", child=True)
 
