570bb8f0efeb01a117c07460e720e46de2c81c84
Robert Sehr <robert.sehr@intranda.com>
2019-09-24 11:51:55 +0200

    changed default folder name for internal process log files

--- Goobi/src/de/sub/goobi/config/ConfigurationHelper.java
+++ Goobi/src/de/sub/goobi/config/ConfigurationHelper.java
@@ -239,7 +239,7 @@ public class ConfigurationHelper implements Serializable {
     }
 
     public String getFolderForInternalProcesslogFiles() {
-        return getLocalString("folder_processlog_internal", "import");
+        return getLocalString("folder_processlog_internal", "intern");
     }
 
     public String getDoneDirectoryName() {
