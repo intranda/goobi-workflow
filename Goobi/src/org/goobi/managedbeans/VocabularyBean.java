f82601b8003426576c4b6cc2b94d82318f492000
Robert Sehr <robert.sehr@intranda.com>
2020-02-12 11:59:02 +0100

    upload excel file, read header, try to assign fields

--- Goobi/src/org/goobi/managedbeans/VocabularyBean.java
+++ Goobi/src/org/goobi/managedbeans/VocabularyBean.java
@@ -1,29 +1,43 @@
 package org.goobi.managedbeans;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.input.BOMInputStream;
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.goobi.vocabulary.Definition;
 import org.goobi.vocabulary.Field;
 import org.goobi.vocabulary.VocabRecord;
 import org.goobi.vocabulary.Vocabulary;
+import org.primefaces.event.FileUploadEvent;
 
 import de.sub.goobi.helper.FacesContextHelper;
 import de.sub.goobi.helper.Helper;
 import de.sub.goobi.persistence.managers.VocabularyManager;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
@@ -61,7 +75,16 @@ public class VocabularyBean extends BasicBean implements Serializable {
     @Getter
     private String[] possibleDefinitionTypes = { "input", "textarea", "select", "select1", "html" };
 
+    @Getter
+    @Setter
+    private Path importFile;
+    @Getter
+    private String filename;
+
     private List<VocabRecord> recordsToDelete;
+
+    @Getter
+    private List<MatchingField> headerOrder;
 
     public VocabularyBean() {
         uiStatus = "down";
@@ -273,8 +296,136 @@ public class VocabularyBean extends BasicBean implements Serializable {
 
     }
 
-    public String uploadRecords() {
+    public void handleFileUpload(FileUploadEvent event) {
+        try {
+            filename = event.getFile().getFileName();
+            copyFile(filename, event.getFile().getInputstream());
 
-        return "";
+        } catch (IOException e) {
+            log.error(e);
+        }
+
+        loadUploadedFile();
     }
+
+    @SuppressWarnings("deprecation")
+    private void loadUploadedFile() {
+        InputStream file = null;
+        try {
+            file = new FileInputStream(importFile.toFile());
+
+            BOMInputStream in = new BOMInputStream(file, false);
+
+            Workbook wb = WorkbookFactory.create(in);
+
+            Sheet sheet = wb.getSheetAt(0);
+
+            Iterator<Row> rowIterator = sheet.rowIterator();
+            //  read and validate first row
+
+            Row headerRow = rowIterator.next();
+
+            int numberOfCells = headerRow.getLastCellNum();
+            headerOrder = new ArrayList<>(numberOfCells);
+            for (int i = 0; i < numberOfCells; i++) {
+                Cell cell = headerRow.getCell(i);
+                if (cell != null) {
+                    cell.setCellType(Cell.CELL_TYPE_STRING);
+                    String value = cell.getStringCellValue();
+                    headerOrder.add(new MatchingField(value, i, CellReference.convertNumToColString(i)));
+                }
+            }
+
+            for (MatchingField mf : headerOrder) {
+                String excelTitle = mf.getColumnHeader();
+                if (excelTitle.matches(".*\\(.*\\)")) {
+                    String titlePart = excelTitle.substring(0, excelTitle.lastIndexOf("(")).trim();
+                    String languagePart = excelTitle.substring(excelTitle.lastIndexOf("(") + 1, excelTitle.lastIndexOf(")")).trim();
+
+                    for (Definition def : currentVocabulary.getStruct()) {
+                        if (def.getLabel().equals(titlePart) && def.getLanguage().equals(languagePart)) {
+                            mf.setAssignedField(def);
+                        }
+                    }
+
+                } else {
+                    String titlePart = excelTitle.trim();
+                    for (Definition def : currentVocabulary.getStruct()) {
+                        if (def.getLabel().equals(titlePart) && StringUtils.isBlank(def.getLanguage())) {
+                            mf.setAssignedField(def);
+                        }
+                    }
+                }
+                // try to detect correct field type
+            }
+
+        } catch (Exception e) {
+            Helper.setFehlerMeldung("file not readable", e);
+            log.error(e);
+        } finally {
+            if (file != null) {
+                try {
+                    file.close();
+                } catch (IOException e) {
+                    log.error(e);
+                }
+            }
+        }
+
+    }
+
+    public void copyFile(String fileName, InputStream in) {
+        OutputStream out = null;
+
+        try {
+            String extension = fileName.substring(fileName.indexOf("."));
+
+            importFile = Files.createTempFile(fileName, extension);
+            out = new FileOutputStream(importFile.toFile());
+
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = in.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+        } catch (IOException e) {
+            log.error(e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    log.error(e);
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    log.error(e);
+                }
+            }
+        }
+    }
+
+    public String uploadRecords() {
+        headerOrder.clear();
+        return "vocabulary_upload";
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    public class MatchingField {
+        @NonNull
+        private String columnHeader;
+        @NonNull
+        private Integer columnOrderNumber;
+        @NonNull
+        private String columnLetter;
+
+        private Definition assignedField;
+    }
+
 }
