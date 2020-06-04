656fe945a9cd1ae3a5a7253eeadfd5b13974e0b6
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2020-06-04 15:05:02 +0200

    fix for mimetype recognition on macs

--- Goobi/src/de/sub/goobi/helper/NIOFileUtils.java
+++ Goobi/src/de/sub/goobi/helper/NIOFileUtils.java
@@ -784,7 +784,7 @@ public class NIOFileUtils implements StorageProviderInterface {
             if (!fileExtension.contains(".")) {
                 return mimeType;
             }
-            fileExtension = fileExtension.substring(fileExtension.lastIndexOf(".")).toLowerCase(); // .tar.gz will not work
+            fileExtension = fileExtension.substring(fileExtension.lastIndexOf(".")+1).toLowerCase(); // .tar.gz will not work
             switch (fileExtension) {
                 case "jpg":
                 case "jpeg":
@@ -828,6 +828,9 @@ public class NIOFileUtils implements StorageProviderInterface {
                     break;
                 case "webm":
                     mimeType = "video/webm" ;
+                    break;
+                case "mov":
+                    mimeType = "video/quicktime" ;
                     break;
                 case "avi":
                     mimeType = "video/x-msvideo";
