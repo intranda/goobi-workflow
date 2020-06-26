987970eebf22b0c188b0a78e94c6f1f320f9a521
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-06-26 16:29:31 +0200

    update GitHub repository name

--- Goobi/src/org/goobi/security/authentication/IAuthenticationProvider.java
+++ Goobi/src/org/goobi/security/authentication/IAuthenticationProvider.java
@@ -8,7 +8,7 @@ import lombok.Getter;
  * Visit the websites for more information.
  *          - https://goobi.io
  *          - https://www.intranda.com
- *          - https://github.com/intranda/goobi
+ *          - https://github.com/intranda/goobi-workflow
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option) any later version.
