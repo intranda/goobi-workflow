848207b4a16e6d3b92efed9a65d271e7c863ae94
Robert Sehr <robert.sehr@intranda.com>
2020-08-19 08:42:21 +0200

    ignore empty fallback folder

--- Goobi/src/org/goobi/beans/Process.java
+++ Goobi/src/org/goobi/beans/Process.java
@@ -458,9 +458,12 @@ public class Process implements Serializable, DatabaseObject, Comparable<Process
             String masterFolder = VariableReplacer.simpleReplace(ConfigurationHelper.getInstance().getProcessImagesMasterDirectoryName(), this);
 
             if (!StorageProvider.getInstance().isDirectory(Paths.get(dir.toString(), masterFolder)) && useFallBack) {
-                String fallback = VariableReplacer.simpleReplace(ConfigurationHelper.getInstance().getProcessImagesFallbackDirectoryName(), this);
-                if (Files.exists(Paths.get(dir.toString(), fallback))) {
-                    masterFolder = fallback;
+                String configuredFallbackFolder = ConfigurationHelper.getInstance().getProcessImagesFallbackDirectoryName();
+                if (StringUtils.isNotBlank(configuredFallbackFolder)) {
+                    String fallback = VariableReplacer.simpleReplace(configuredFallbackFolder, this);
+                    if (Files.exists(Paths.get(dir.toString(), fallback))) {
+                        masterFolder = fallback;
+                    }
                 }
             }
 
@@ -1865,9 +1868,6 @@ public class Process implements Serializable, DatabaseObject, Comparable<Process
                     folderList.add(new SelectItem(getImagesOrigDirectory(false), Helper.getTranslation("process_log_file_masterFolder")));
                 }
 
-
-
-
                 Iterator<String> configuredImageFolder = ConfigurationHelper.getInstance().getLocalKeys("process.folder.images");
                 while (configuredImageFolder.hasNext()) {
                     String keyName = configuredImageFolder.next();
@@ -1883,7 +1883,6 @@ public class Process implements Serializable, DatabaseObject, Comparable<Process
                         //                        folderList.add(new SelectItem(getConfiguredImageFolder(folderName), Helper.getTranslation(folderName)));
                     }
                 }
-
 
             } catch (SwapException | DAOException | IOException | InterruptedException e) {
                 logger.error(e);
@@ -2247,7 +2246,7 @@ public class Process implements Serializable, DatabaseObject, Comparable<Process
     public String getConfiguredImageFolder(String folderName) throws IOException, InterruptedException, SwapException, DAOException {
         if ("master".equals(folderName)) {
             return getImagesOrigDirectory(false);
-        } else if ("main".equals(folderName)|| "media".equals(folderName)) {
+        } else if ("main".equals(folderName) || "media".equals(folderName)) {
             return getImagesTifDirectory(false);
         }
 
