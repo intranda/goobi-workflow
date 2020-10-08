b899f0c4ccd8139e17e71e2baf9909c6bbc24547
maurice <maurice@maurice-PC>
2020-10-08 14:05:06 +0200

    Added class RestUserInfo, began to implement User list

--- Goobi/src/org/goobi/api/rest/CurrentUsers.java
+++ Goobi/src/org/goobi/api/rest/CurrentUsers.java
@@ -1,28 +1,132 @@
 package org.goobi.api.rest;
 
-import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.FactoryFinder;
+import javax.faces.application.Application;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.context.FacesContextFactory;
+import javax.faces.el.ValueBinding;
+import javax.faces.lifecycle.Lifecycle;
+import javax.faces.lifecycle.LifecycleFactory;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.goobi.api.rest.model.RestUserInfo;
+
+import de.sub.goobi.forms.SessionForm;
+import de.sub.goobi.helper.Helper;
 
 // Access with http://localhost:8080/goobi/api/currentusers 
 @Path("/currentusers")
 public class CurrentUsers {
 
+    @Context
+    private HttpServletRequest servletRequest;
+    @Context
+    private HttpServletResponse servletResponse;
+
+    private FacesContext facesContext;
+    private SessionForm sessionForm;
+
     @GET
-    //@Path("/currentusers/json")
     @Produces(MediaType.APPLICATION_JSON)
-    public String[] getCurrentUsers() {
-        String[] currentUsers = new String[2];
-        currentUsers[0] = "Linus";
-        currentUsers[1] = "Tux";
-        return currentUsers;
+    public RestUserInfo[] getCurrentUsers() {
+        // Try to get the sessionForm directly
+        this.sessionForm = (SessionForm) (Helper.getManagedBeanValue("#{SessionForm}"));
+
+        // Go the longer but saver way
+        if (this.sessionForm == null) {
+            // Prepare the facesContext depending on how this is possible
+            this.initFacesContext();
+
+            // Get the current application and valueBinding 
+            Application application = this.facesContext.getApplication();
+            ValueBinding valueBinding = application.createValueBinding("#{SessionForm}");
+            //ExpressionFactory expressionFactory = application.getExpressionFactory();
+            //ValueExpression valueExpression = expressionFactory.createValueExpression("#{SessionForm}", null);
+
+            // Set the sessionForm on alternative way
+            this.sessionForm = (SessionForm) (valueBinding.getValue(this.facesContext));
+
+        }
+
+        // Now return the expected list of users / sessions
+        // Otherwise sessionForm stills null, this is a warning
+        if (this.sessionForm != null) {
+            return this.generateUserList();
+
+        }
+        /*
+        else {
+            List<String> l = new ArrayList<String>();
+            l.add("Any Exception");
+            l.add("facesContext=" + this.facesContext);
+            l.add("sessionForm=" + this.sessionForm);
+            l.add("Currently there might be no users!");
+            return l;
+        }
+        */
     }
     
-    @GET
-    //@Path("/currentusers/html")
-    @Produces(MediaType.TEXT_HTML)
-    public String getCurrentUsersHTML() {
-        return "<!DOCTYPE html><html><head><title>Current users</title></head><body><ul><li>Linus</li><li>Tux</li></ul></body></html>";
+    private RestUserInfo[] generateUserList() {
+
+        List<Map> users = this.sessionForm.getAlleSessions();
+        int length = users.size();
+        RestUserInfo[] rui = new RestUserInfo[length];
+        int x = 0;
+        while (x < length) {
+            rui[x] = new RestUserInfo();
+            Map userMap = users.get(x);
+            Collection userCollection = userMap.values();
+            Object[] userObject = userCollection.toArray();
+            String[] userString = (String[])(userObject);
+            rui.setUser(user.getUser());
+            rui.setAddress(user.getUser());
+            rui.setBrowser(user.getBrowser());
+            rui.setCreated(user.getCreated());
+            rui.setLast(user.getLast());
+            x++;
+        }
+        return rui;
+    }
+
+    private void initFacesContext() {
+        // Try to get the current instance to initialize the facesContext
+        this.facesContext = FacesContext.getCurrentInstance();
+
+        // When this failed, the longer way with FacesContextFactory has to be done
+        if (this.facesContext == null) {
+
+            // Create the factory and set facesContext
+            FacesContextFactory contextFactory = (FacesContextFactory) FactoryFinder.getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
+            LifecycleFactory lifecycleFactory = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
+            Lifecycle lifecycle = lifecycleFactory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
+            this.facesContext = contextFactory.getFacesContext(this.servletRequest.getSession().getServletContext(), this.servletRequest,
+                    this.servletResponse, lifecycle);
+
+            // Set this as current instance using our inner class (because it wasn't available above)
+            InnerFacesContext.setFacesContextAsCurrentInstance(this.facesContext);
+
+            // Set a new viewRoot, otherwise context.getViewRoot returns null
+            UIViewRoot view = this.facesContext.getApplication().getViewHandler().createView(this.facesContext, "");
+            this.facesContext.setViewRoot(view);
+        }
+    }
+
+    // You need an inner class to be able to call FacesContext.setCurrentInstance
+    // since it's a protected method
+    private abstract static class InnerFacesContext extends FacesContext {
+        protected static void setFacesContextAsCurrentInstance(FacesContext facesContext) {
+            FacesContext.setCurrentInstance(facesContext);
+        }
     }
 }
