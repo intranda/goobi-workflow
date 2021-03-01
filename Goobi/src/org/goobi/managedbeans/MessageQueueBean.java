6b9cd45221d97f6af4ccb247a9ca340a56a57cd2
Robert Sehr <robert.sehr@intranda.com>
2021-03-01 16:01:05 +0100

    show queue overview, details for each queue type, cancel single message

--- Goobi/src/org/goobi/managedbeans/MessageQueueBean.java
+++ Goobi/src/org/goobi/managedbeans/MessageQueueBean.java
@@ -16,7 +16,6 @@ import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.management.MBeanServerConnection;
 import javax.management.MBeanServerInvocationHandler;
-import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
@@ -26,6 +25,8 @@ import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.jmx.QueueViewMBean;
 import org.apache.activemq.command.ActiveMQTextMessage;
+import org.apache.commons.lang.StringUtils;
+import org.goobi.api.mq.MessageStatus;
 import org.goobi.api.mq.TaskTicket;
 
 import com.google.gson.Gson;
@@ -61,6 +62,14 @@ public class MessageQueueBean extends BasicBean implements Serializable {
 
     @Getter
     private boolean messageBrokerStart;
+
+    @Getter
+    @Setter
+    private String messageType;
+
+    @Getter
+    @Setter
+    private String queueType;
 
     public MessageQueueBean() {
         this.initMessageBrokerStart();
@@ -144,13 +153,13 @@ public class MessageQueueBean extends BasicBean implements Serializable {
      */
 
     public void deleteSlowQueryTicket(TaskTicket ticket) {
-        try {
-            ObjectName queue =
-                    new ObjectName("org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=goobi_slow");
-            deleteMessage(ticket.getMessageId(), queue);
-        } catch (MalformedObjectNameException e1) {
-            log.error(e1);
-        }
+        //        try {
+        //            ObjectName queue =
+        //                    new ObjectName("org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=goobi_slow");
+        //            deleteMessage(ticket.getMessageId(), queue);
+        //        } catch (MalformedObjectNameException e1) {
+        //            log.error(e1);
+        //        }
     }
 
     /**
@@ -179,28 +188,30 @@ public class MessageQueueBean extends BasicBean implements Serializable {
      */
 
     public List<TaskTicket> getActiveSlowQueryMesssages() {
+
         List<TaskTicket> answer = new ArrayList<>();
         if (!ConfigurationHelper.getInstance().isStartInternalMessageBroker()) {
             return answer;
         }
-        try {
-            connection.start();
-            Queue queue = queueSession.createQueue("goobi_slow");
-            QueueBrowser browser = queueSession.createBrowser(queue);
-            Enumeration<?> messagesInQueue = browser.getEnumeration();
-            while (messagesInQueue.hasMoreElements()) {
-                ActiveMQTextMessage queueMessage = (ActiveMQTextMessage) messagesInQueue.nextElement();
+        if (StringUtils.isNotBlank(messageType)) {
+            try {
+                connection.start();
+                Queue queue = queueSession.createQueue("goobi_slow");
+                QueueBrowser browser = queueSession.createBrowser(queue, "ticketType = '" + messageType + "'");
+                Enumeration<?> messagesInQueue = browser.getEnumeration();
+                while (messagesInQueue.hasMoreElements()) {
+                    ActiveMQTextMessage queueMessage = (ActiveMQTextMessage) messagesInQueue.nextElement();
 
-                TaskTicket ticket = gson.fromJson(queueMessage.getText(), TaskTicket.class);
-                ticket.setMessageId(queueMessage.getJMSMessageID());
+                    TaskTicket ticket = gson.fromJson(queueMessage.getText(), TaskTicket.class);
+                    ticket.setMessageId(queueMessage.getJMSMessageID());
 
-                answer.add(ticket);
+                    answer.add(ticket);
+                }
+                connection.stop();
+            } catch (JMSException e) {
+                log.error(e);
             }
-            connection.stop();
-        } catch (JMSException e) {
-            log.error(e);
         }
-
         return answer;
     }
 
@@ -210,13 +221,13 @@ public class MessageQueueBean extends BasicBean implements Serializable {
      * @param ticket to delete
      */
     public void deleteFastQueryTicket(TaskTicket ticket) {
-        try {
-            ObjectName queue =
-                    new ObjectName("org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=goobi_fast");
-            deleteMessage(ticket.getMessageId(), queue);
-        } catch (MalformedObjectNameException e1) {
-            log.error(e1);
-        }
+        //        try {
+        //            ObjectName queue =
+        //                    new ObjectName("org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=goobi_fast");
+        //            deleteMessage(ticket.getMessageId(), queue);
+        //        } catch (MalformedObjectNameException e1) {
+        //            log.error(e1);
+        //        }
     }
 
     /**
@@ -248,37 +259,47 @@ public class MessageQueueBean extends BasicBean implements Serializable {
         if (!ConfigurationHelper.getInstance().isStartInternalMessageBroker()) {
             return answer;
         }
-        try {
-            connection.start();
-            Queue queue = queueSession.createQueue("goobi_fast");
-            QueueBrowser browser = queueSession.createBrowser(queue);
-            Enumeration<?> messagesInQueue = browser.getEnumeration();
-            while (messagesInQueue.hasMoreElements()) {
-                ActiveMQTextMessage queueMessage = (ActiveMQTextMessage) messagesInQueue.nextElement();
+        if (StringUtils.isNotBlank(messageType)) {
+            try {
+                connection.start();
+                Queue queue = queueSession.createQueue("goobi_fast");
+                QueueBrowser browser = queueSession.createBrowser(queue, "ticketType = '" + messageType + "'");
+                Enumeration<?> messagesInQueue = browser.getEnumeration();
+                while (messagesInQueue.hasMoreElements()) {
+                    ActiveMQTextMessage queueMessage = (ActiveMQTextMessage) messagesInQueue.nextElement();
 
-                TaskTicket ticket = gson.fromJson(queueMessage.getText(), TaskTicket.class);
-                ticket.setMessageId(queueMessage.getJMSMessageID());
+                    TaskTicket ticket = gson.fromJson(queueMessage.getText(), TaskTicket.class);
+                    ticket.setMessageId(queueMessage.getJMSMessageID());
 
-                answer.add(ticket);
+                    answer.add(ticket);
+                }
+                connection.stop();
+            } catch (JMSException e) {
+                log.error(e);
             }
-            connection.stop();
-        } catch (JMSException e) {
-            log.error(e);
         }
 
         return answer;
     }
 
-    private void deleteMessage(String messageId, ObjectName queueName) {
-        try {
-            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi");
-            JMXConnector connector = JMXConnectorFactory.connect(url, null);
-            connector.connect();
-            MBeanServerConnection connection = connector.getMBeanServerConnection();
-            QueueViewMBean bean = MBeanServerInvocationHandler.newProxyInstance(connection, queueName, QueueViewMBean.class, true);
-            bean.removeMessage(messageId);
-        } catch (Exception e) {
-            log.error(e);
-        }
+    public void deleteMessage(TaskTicket ticket) {
+        MessageStatus.cancelMessage(ticket.getMessageId(), queueType, messageType, ticket.getProcessId());
+
+        //        try {
+        //
+        //            Queue queue = queueSession.createQueue(queueType);
+        //            MessageConsumer consumer =
+        //                    queueSession.createConsumer(queue, "JMSMessageID=" + ticket.getMessageId().replace("ID:", "").replace(":1:1:1:1", ""));
+        //            //            MessageConsumer consumer = queueSession.createConsumer(dest, "ticketType='" + messageType + "' AND processid=" +ticket.getProcessId());
+        //            connection.start();
+        //            Message message = consumer.receiveNoWait();
+        //            if (message != null) {
+        //                message.acknowledge();
+        //            }
+        //            connection.stop();
+        //        } catch (JMSException e) {
+        //            log.error(e);
+        //        }
     }
+
 }
