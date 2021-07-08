57ed92cef8fff92f95f19ddbd39195502396b64c
Oliver Paetzel <oliver.paetzel@intranda.com>
2021-07-08 14:52:33 +0200

    use "false" instead of "0" for boolean field in "benutzer" table

--- Goobi/src/de/sub/goobi/persistence/managers/UserMysqlHelper.java
+++ Goobi/src/de/sub/goobi/persistence/managers/UserMysqlHelper.java
@@ -336,7 +336,7 @@ class UserMysqlHelper implements Serializable {
                 StringBuilder deactivateUserQuery = new StringBuilder();
                 deactivateUserQuery.append("UPDATE benutzer SET ");
                 deactivateUserQuery.append("isVisible = 'deleted', ");
-                deactivateUserQuery.append("IstAktiv = 0, ");
+                deactivateUserQuery.append("IstAktiv = false, ");
                 deactivateUserQuery.append("login= 'deletedUser" + ro.getId() + "', ");
                 deactivateUserQuery.append("email = '', ");
                 deactivateUserQuery.append("Vorname ='deleted', ");
