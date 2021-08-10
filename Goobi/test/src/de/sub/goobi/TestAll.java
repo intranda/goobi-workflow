f0cd233dd833c69a6d370912ec0d412943aa9ce0
Robert Sehr <robert.sehr@intranda.com>
2021-08-10 15:45:26 +0200

    junit test for vocabulary conversion

--- Goobi/test/src/de/sub/goobi/TestAll.java
+++ Goobi/test/src/de/sub/goobi/TestAll.java
@@ -1,5 +1,6 @@
 package de.sub.goobi;
 
+import org.goobi.vocabulary.helper.ImportJsonVocabulary;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -11,10 +12,12 @@ import io.goobi.workflow.xslt.GeneratePdfFromXsltTest;
 @RunWith(Suite.class)
 
 @SuiteClasses({ OldTests.class, de.sub.goobi.config.TestAll.class, de.sub.goobi.export.dms.TestAll.class, de.sub.goobi.converter.TestAll.class,
-        de.sub.goobi.forms.TestAll.class, org.goobi.api.mq.TestQueueType.class, de.sub.goobi.export.download.TestAll.class,
-        de.sub.goobi.helper.TestAll.class, de.sub.goobi.helper.enums.TestAll.class, ExceptionTest.class, de.sub.goobi.helper.ldap.TestAll.class,
-        de.sub.goobi.helper.servletfilter.TestAll.class, de.sub.goobi.metadaten.TestAll.class, org.goobi.api.rest.TestRestConfig.class,
-        org.goobi.api.rest.TestAuthorizationFilter.class, LockingBeanTest.class, GeneratePdfFromXsltTest.class, org.goobi.api.display.TestAll.class })
+    de.sub.goobi.forms.TestAll.class, org.goobi.api.mq.TestQueueType.class, de.sub.goobi.export.download.TestAll.class,
+    de.sub.goobi.helper.TestAll.class, de.sub.goobi.helper.enums.TestAll.class, ExceptionTest.class, de.sub.goobi.helper.ldap.TestAll.class,
+    de.sub.goobi.helper.servletfilter.TestAll.class, de.sub.goobi.metadaten.TestAll.class, org.goobi.api.rest.TestRestConfig.class,
+    org.goobi.api.rest.TestAuthorizationFilter.class, LockingBeanTest.class, GeneratePdfFromXsltTest.class, org.goobi.api.display.TestAll.class,
+    ImportJsonVocabulary.class
+})
 public class TestAll {
 
 }
