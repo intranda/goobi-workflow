0f9eb3cb615daaff275ad6c8ed2c7aefa6b40392
Maxim Strzebkowski <maxim.strzebkowski@intranda.com>
2021-12-21 11:06:20 +0100

    Added ruleset option to column configuration dropdown in process table

--- Goobi/src/org/goobi/managedbeans/ProcessBean.java
+++ Goobi/src/org/goobi/managedbeans/ProcessBean.java
@@ -324,7 +324,7 @@ public class ProcessBean extends BasicBean implements Serializable {
             this.anzeigeAnpassen.put("processId", login.getMyBenutzer().isDisplayIdColumn());
             this.anzeigeAnpassen.put("batchId", login.getMyBenutzer().isDisplayBatchColumn());
             this.anzeigeAnpassen.put("processDate", login.getMyBenutzer().isDisplayProcessDateColumn());
-
+            this.anzeigeAnpassen.put("processRuleset", login.getMyBenutzer().isDisplayRulesetColumn());
             this.anzeigeAnpassen.put("thumbnail", login.getMyBenutzer().isDisplayThumbColumn());
             this.anzeigeAnpassen.put("metadatadetails", login.getMyBenutzer().isDisplayMetadataColumn());
             this.anzeigeAnpassen.put("gridview", login.getMyBenutzer().isDisplayGridView());
@@ -350,6 +350,7 @@ public class ProcessBean extends BasicBean implements Serializable {
             this.anzeigeAnpassen.put("batchId", false);
             this.anzeigeAnpassen.put("processDate", false);
             anzeigeAnpassen.put("institution", false);
+            this.anzeigeAnpassen.put("processRuleset",false);
         }
         DONEDIRECTORYNAME = ConfigurationHelper.getInstance().getDoneDirectoryName();
 
