bb002167607d664fb0cac490484290877d38286e
Robert Sehr <robert.sehr@intranda.com>
2022-02-17 15:35:57 +0100

    allow fullscreen dashboards

--- Goobi/src/org/goobi/managedbeans/DashboardBean.java
+++ Goobi/src/org/goobi/managedbeans/DashboardBean.java
@@ -1,17 +1,21 @@
 package org.goobi.managedbeans;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import org.apache.commons.lang.StringUtils;
 import org.goobi.beans.User;
+import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.PluginLoader;
 import org.goobi.production.plugin.interfaces.IDashboardPlugin;
 
+import de.sub.goobi.helper.FacesContextHelper;
 import de.sub.goobi.helper.Helper;
 import lombok.Getter;
 
@@ -42,7 +46,17 @@ public class DashboardBean implements Serializable {
             IDashboardPlugin plugin = (IDashboardPlugin) PluginLoader.getPluginByTitle(PluginType.Dashboard, pluginName);
             if (plugin != null) {
                 this.plugin = plugin;
+                if (PluginGuiType.FULL == plugin.getPluginGuiType()) {
+                    ExternalContext ec = FacesContextHelper.getCurrentFacesContext().getExternalContext();
+                    try {
+                        ec.redirect(ec.getRequestContextPath() + plugin.getGuiPath());
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
             }
+
         }
     }
 }
