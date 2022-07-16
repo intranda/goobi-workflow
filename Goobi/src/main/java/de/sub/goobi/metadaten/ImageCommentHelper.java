48ae803f15992c3c81d28cd458f57b2224ebbf1f
janos <janos.seboek@intranda.com>
2022-07-16 11:25:34 +0200

    Bug #22934 - made ImageCommentHelper more robust, moved image comments a folder up

--- Goobi/src/main/java/de/sub/goobi/metadaten/ImageCommentHelper.java
+++ Goobi/src/main/java/de/sub/goobi/metadaten/ImageCommentHelper.java
@@ -19,96 +19,106 @@ import lombok.extern.log4j.Log4j2;
  * @author joel
  *
  */
+
 @Log4j2
 public class ImageCommentHelper {
 
     private static Gson gson = new Gson();
-    //dictionary of comment files, so each is only read once
     private HashMap<String, ImageComments> commentFiles;
+    private final String PATH_ADJUSTMENT = "../";
+    private final String COMMENTS_FILE_NAME = "comments";
+    private final String JSON_EXTENSION = ".json";
 
     public ImageCommentHelper() {
-
         commentFiles = new HashMap<>();
-
     }
 
     private ImageComments getCommentFile(String imageFolderName) {
-
         if (!commentFiles.containsKey(imageFolderName)) {
             try {
-
-                String strCommentFile = imageFolderName + "imageComments.json";
-
+                final String strCommentFile = buildCommentPath(imageFolderName);
                 File commentsFile = new File(strCommentFile);
-
                 if (commentsFile.exists()) {
-
                     BufferedReader br;
                     br = new BufferedReader(new FileReader(strCommentFile));
                     ImageComments commentsClassNew = gson.fromJson(br, ImageComments.class);
-
                     commentFiles.put(imageFolderName, commentsClassNew);
-
                 } else {
-
                     ImageComments commentsClassNew = new ImageComments();
                     commentFiles.put(imageFolderName, commentsClassNew);
                 }
-
             } catch (FileNotFoundException e) {
                 log.error(e);
             }
         }
-
         return commentFiles.get(imageFolderName);
     }
 
     public String getComment(String imageFolderName, String imageName) {
-
         ImageComments commentsClass = getCommentFile(imageFolderName);
-
         return commentsClass.getComment(imageName);
-
     }
 
     public void setComment(String imageFolderName, String imageName, String comment) {
-
         ImageComments commentsClass = getCommentFile(imageFolderName);
         commentsClass.setComment(imageName, comment);
-
-        try (Writer writer = new FileWriter(imageFolderName + "imageComments.json")) {
+        final String strCommentFile = buildCommentPath(imageFolderName);
+        try (Writer writer = new FileWriter(strCommentFile)) {
             gson.toJson(commentsClass, writer);
         } catch (IOException e) {
             log.error(e);
         }
-
     }
 
     public HashMap<String, String> getComments(String imageFolderName) {
-
         return getCommentFile(imageFolderName).comments;
+    }
+    
+    private final String buildCommentPath(String folderName) {
+        final String type = getType(folderName);
+        StringBuilder commentPath = new StringBuilder(appendSlash(folderName)); 
+        commentPath.append(PATH_ADJUSTMENT);
+        commentPath.append(COMMENTS_FILE_NAME);
+        commentPath.append(type);
+        commentPath.append(JSON_EXTENSION);
+        return commentPath.toString();
+    }
+    
+    // Determine whether we are dealing with media or master
+    private String getType(String path) {
+        if (path == null) {
+            return null;
+        }
+        String folderName = appendSlash(path);
+        int lastUnderscoreIndex = folderName.lastIndexOf("_");
+        if (lastUnderscoreIndex >= 0 && folderName.length() > 0) {
+            return folderName.substring(lastUnderscoreIndex, folderName.length() - 1);
+        } else {
+            return "";
+        }
+    }
+
+    private String appendSlash(String folderName) {
+        if (!folderName.endsWith("/")) {
+            folderName += ("/");
+        }
+        return folderName;
     }
 
     public class ImageComments {
-
-        public HashMap<String, String> comments;
-
+        private HashMap<String, String> comments;
+ 
         public ImageComments() {
             comments = new HashMap<>();
         }
 
         public String getComment(String imageName) {
-
             return comments.get(imageName);
         }
 
         public void setComment(String imageName, String comment) {
-
-            //            String currentUser = Helper.getCurrentUser().getNachVorname();
-            //            Date dateNow = new Date();
             comments.put(imageName, comment);
         }
-
     }
 
 }
