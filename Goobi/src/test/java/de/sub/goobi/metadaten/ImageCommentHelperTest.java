45964319ad6c6ca6dfe0dd0ca5534b144edbae00
janos <janos.seboek@intranda.com>
2022-07-16 13:20:22 +0200

    A few tests, just in case.

---
+++ Goobi/src/test/java/de/sub/goobi/metadaten/ImageCommentHelperTest.java
@@ -0,0 +1,45 @@
+package de.sub.goobi.metadaten;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+/**
+ * @author janos
+ *
+ */
+
+public class ImageCommentHelperTest {
+    
+    private ImageCommentHelper commentHelper = new ImageCommentHelper();
+    final File path = new File("src/test/resources/metadata/99/images/testprocess_media");
+    final String imageFolderName = path.getAbsolutePath();
+    
+    @Test
+    public void getCommentTest() {
+        final String imageName = "00000001.tif";
+        final String comment = commentHelper.getComment(imageFolderName, imageName);
+        assertEquals("MyTestComment", comment);
+    }
+    
+    @Test
+    public void getCommentsTest() {
+        final String imageName = "00000001.tif";
+        HashMap<String, String> comments = commentHelper.getComments(imageFolderName);
+        assertEquals(2, comments.size());
+        assertEquals("MyTestComment", comments.get(imageName));
+    }
+    
+    @Test
+    public void setCommentTest() {
+        final String imageName = "00000002.tif";
+        commentHelper.setComment(imageFolderName, imageName, "MyChangedComment");
+        final String newComment = commentHelper.getComment(imageFolderName, imageName);
+        assertEquals("MyChangedComment", newComment);
+        commentHelper.setComment(imageFolderName, imageName, "");
+    }
+    
+}
