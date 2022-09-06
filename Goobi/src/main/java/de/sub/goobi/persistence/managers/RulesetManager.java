7930deba791e127dd35596de000c3f91f816177d
zehong <zehong.hu@intranda.com>
2022-09-06 16:04:56 +0200

    remove tautology from de.sub.goobi.persistence.managers.RulesetManager

--- Goobi/src/main/java/de/sub/goobi/persistence/managers/RulesetManager.java
+++ Goobi/src/main/java/de/sub/goobi/persistence/managers/RulesetManager.java
@@ -118,13 +118,11 @@ public class RulesetManager implements IManager, Serializable {
         @Override
         public Ruleset handle(ResultSet rs) throws SQLException {
             try {
-                if (rs.next()) {
+                if (rs.next()) { // implies that rs != null
                     return convert(rs);
                 }
             } finally {
-                if (rs != null) {
-                    rs.close();
-                }
+                rs.close();
             }
             return null;
         }
@@ -137,15 +135,11 @@ public class RulesetManager implements IManager, Serializable {
             List<Ruleset> answer = new ArrayList<>();
             try {
                 while (rs.next()) {
-                    Ruleset o = convert(rs);
-                    if (o != null) {
-                        answer.add(o);
-                    }
+                    Ruleset o = convert(rs); // implies that o != null
+                    answer.add(o);
                 }
             } finally {
-                if (rs != null) {
-                    rs.close();
-                }
+                rs.close();
             }
             return answer;
         }
