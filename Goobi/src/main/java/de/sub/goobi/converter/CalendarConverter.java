13c33d490a4ba98a3faa8c34f8d27d625eb46b6f
janos <janos.seboek@intranda.com>
2022-10-17 22:11:14 +0200

    Conversion now working, tests in working condition - refs #23730

--- Goobi/src/main/java/de/sub/goobi/converter/CalendarConverter.java
+++
@@ -1,7 +0,0 @@
-package de.sub.goobi.converter;
-
-public class CalendarConverter {
-    public static void convert() {
-
-    }
-}
--- Goobi/src/main/java/de/sub/goobi/metadaten/MetaConvertibleDate.java
+++ Goobi/src/main/java/de/sub/goobi/metadaten/MetaConvertibleDate.java
@@ -1,13 +1,25 @@
 package de.sub.goobi.metadaten;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import org.threeten.extra.chrono.BritishCutoverDate;
+import org.threeten.extra.chrono.JulianDate;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
 public class MetaConvertibleDate {
     public enum DateType {
         JULIAN,
         BRITISH,
-        GEORGIAN;
+        GREGORIAN;
     }
 
     // Maybe use a java.time class instead of a string for compatibility?
+    @Getter
     private String date;
     private DateType type;
 
@@ -24,14 +36,56 @@ public class MetaConvertibleDate {
         return this.type == DateType.BRITISH ? true : false;
     }
 
-    public boolean isGeorgian() {
-        return this.type == DateType.GEORGIAN ? true : false;
+    public boolean isGregorian() {
+        return this.type == DateType.GREGORIAN ? true : false;
     }
 
-    // impl. concrete flex. date
-    // TODO: types of conversion
+    // TODO: real docstring
+    // When called, returns a new MetaConvertibleDate of the desired type.
     public MetaConvertibleDate convert(DateType toType) {
-        MetaConvertibleDate new_date = new MetaConvertibleDate(this.date, toType);
-        return new_date;
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+        MetaConvertibleDate returnDate = new MetaConvertibleDate("0000-01-01", DateType.GREGORIAN);
+        if (type == toType) {
+            returnDate = new MetaConvertibleDate(date, toType);
+        }
+        LocalDate normalizedDate = LocalDate.from(dateFormatter.parse(date));
+        final int year = normalizedDate.getYear();
+        final int month = normalizedDate.getMonthValue();
+        final int day = normalizedDate.getDayOfMonth();
+        if (type == DateType.BRITISH && toType == DateType.GREGORIAN) {
+            BritishCutoverDate britishDate = BritishCutoverDate.of(year, month, day);
+            LocalDate gregorianDate = LocalDate.parse(britishDate.format(dateFormatter));
+            returnDate = new MetaConvertibleDate(gregorianDate.format(dateFormatter), DateType.GREGORIAN);
+        }
+        if (type == DateType.GREGORIAN && toType == DateType.BRITISH) {
+            BritishCutoverDate britishDate = BritishCutoverDate.from(normalizedDate);
+            returnDate = new MetaConvertibleDate(britishDate.format(dateFormatter), DateType.BRITISH);
+        }
+        if (type == DateType.JULIAN && toType == DateType.GREGORIAN) {
+            JulianDate julianDate = JulianDate.of(year, month, day);
+            LocalDate gregorianDate = LocalDate.from(julianDate);
+            returnDate = new MetaConvertibleDate(gregorianDate.format(dateFormatter), DateType.GREGORIAN);
+        }
+        if (type == DateType.GREGORIAN && toType == DateType.JULIAN) {
+            JulianDate julianDate = JulianDate.from(normalizedDate);
+            returnDate = new MetaConvertibleDate(julianDate.format(dateFormatter), DateType.JULIAN);
+        }
+        return returnDate;
+    }
+
+    // // B2G, G2B, J2G, G2J
+    ///
+    // LocalDate ld = LocalDate.parse(date);
+    // JulianDate jd = JulianDate.from(ld);
+    // BritishCutoverDate bd = BritishCutoverDate.from(ld);
+    //
+    public boolean isValid() {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+        try {
+            dateFormatter.parse(date);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
     }
 }
