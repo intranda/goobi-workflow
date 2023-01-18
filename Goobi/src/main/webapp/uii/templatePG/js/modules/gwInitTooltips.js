f14e0d231f35ea7c7de90390162e5fc0689948e2
Jakob Epler <jakob.epler@intranda.com>
2023-01-18 12:35:54 +0100

    Copy includes for migration and move inline JS into modules; refs #22612

---
+++ Goobi/src/main/webapp/uii/templatePG/js/modules/gwInitTooltips.js
@@ -0,0 +1,46 @@
+/**
+ * @description Destroy ALL tooltips and initialize again.  
+ * 
+ */
+
+var gwInitTooltips = ( function() {
+    'use strict';
+    
+    var _debug = true;
+    var _defaults = {
+      scope: document,
+      selectors: '[data-bs-toggle="tooltip"], [data-toggle="tooltip"]',
+    }
+
+    function init({ scope = _defaults.scope } = {}) {
+      _initTooltips(scope);
+    }
+
+    /** @description find and return all elements that have a tooltip */
+    function _getTooltipTriggers(scope) {
+      var tooltipTriggerList = 
+        [].slice.call(
+          scope.querySelectorAll(_defaults.selectors)
+        )
+      return tooltipTriggerList 
+    }
+
+    /** @description Initialize tooltips.  */ 
+    function _initTooltips(scope) {
+      const tooltipTriggers = _getTooltipTriggers(scope)
+
+      // Initialize tooltips
+      tooltipTriggers.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+      })
+
+      // Debugging
+      if (_debug) console.log('%c### called gwInitTooltips._initTooltips.js ###', 'color: #368ee0')
+      if (_debug) console.log({ tooltipTriggers })
+    }
+
+    return {
+      init 
+    }
+
+} )();
