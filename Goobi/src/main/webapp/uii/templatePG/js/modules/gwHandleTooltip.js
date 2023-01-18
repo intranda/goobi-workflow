ca7d6aee89dfad582a074ad490090322a05f4576
Jakob Epler <jakob.epler@intranda.com>
2023-01-18 15:08:36 +0100

    task_all.xhtml: Migrate to BS5; refs #24279

--- Goobi/src/main/webapp/uii/templatePG/js/modules/gwHandleTooltip.js
+++ Goobi/src/main/webapp/uii/templatePG/js/modules/gwHandleTooltip.js
@@ -5,28 +5,37 @@
 var gwHandleTooltip = ( function() {
     'use strict';
     
-    var _debug = false;
+    var _debug = true;
+    var _defaultSelector = '[data-goobi="handle-tooltip"]'
 
-    function init(data) {
-      _handleTooltip(data);
+    function init(data, { selector = _defaultSelector, restore = true } = {})  {
+      // get the element triggering the ajax call
+      const trigger = bootstrap.Tooltip.getInstance(data.source)
+      _removeTooltip(trigger);
+
+      // Resotre tooltips after ajax is done
+      if(!restore) return
+        _restoreTooltip(data, selector);
 
     }
 
-    function _handleTooltip(data) {
-      if (_debug) console.log('%c### called gwHandleTooltip.js ###', 'color: #368ee0')
+    function _removeTooltip(trigger) {
+      if (_debug) console.log('%c## called gwHandleTooltip._removeTooltip ##', 'color: #368ee0')
+      trigger?.tip?.remove();
+    }
 
-      // get the element triggering the ajax call
-      const trigger = bootstrap.Tooltip.getInstance(data.source)
-
-      // remove associated tooltips
-      if (trigger) trigger?.tip?.remove();
+    function _restoreTooltip(data, selector) {
+      if (_debug) console.log('%c### called gwHandleTooltip._restoreTooltip ###', 'color: orange')
 
       // if ajax call is done, init a new tooltip
       if (data.status === 'success') {
         // get the rerendered trigger element
-        const tooltipTrigger = document.querySelector('[data-goobi="handle-tooltip"]')
-        // fire up a tooltip (new instances are enabled by default)
-        new bootstrap.Tooltip(tooltipTrigger)
+        const tooltipTriggers = document.querySelectorAll(selector)
+
+        // fire up new tooltip instances (new instances are enabled by default)
+        tooltipTriggers.forEach(trigger => new bootstrap.Tooltip(trigger))
+
+        if (_debug) console.log(tooltipTriggers)
       }
     }
 
