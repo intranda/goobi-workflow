02d4639b5e6621d85116748ff77f32151ec3df60
Maurice Müller <maurice.mueller@intranda.com>
2023-03-20 09:21:20 +0100

    Moved project title method refs #23880

--- Goobi/src/main/java/org/goobi/beans/Project.java
+++ Goobi/src/main/java/org/goobi/beans/Project.java
@@ -394,7 +394,7 @@ public class Project extends AbstractJournal implements Serializable, DatabaseOb
         setNumberOfVolumes(source.getNumberOfVolumes());
         setProjectIsArchived(source.getProjectIsArchived());
         setStartDate(source.getStartDate());
-        setTitel(ProjectManager.cloneProjectTitleWithoutNameConflict(source.getTitel()));
+        setTitel(Project.cloneProjectTitleWithoutNameConflict(source.getTitel()));
         setUseDmsImport(source.isUseDmsImport());
         setInstitution(source.getInstitution());
         setProjectIdentifier(source.getProjectIdentifier());
@@ -437,4 +437,28 @@ public class Project extends AbstractJournal implements Serializable, DatabaseOb
     public Path getDownloadFolder() {
         return Paths.get(ConfigurationHelper.getInstance().getGoobiFolder(), "uploads", "project", titel.replace(" ", "_"));
     }
+
+    /**
+     * Returns a project title for the case that the project with the given source project title is cloned. The returned title is generated so that is
+     * unused in the current list of (active and inactive) projects in the goobi database. If a normal project title is cloned for the first time,
+     * this method returns sourceProjectTitle + "_copy". If there is already a project with that title, an index is added to the title
+     * (sourceProjectTitle + "_copy_" + index) until there is no project with that name.
+     *
+     * @param sourceProjectTitle The title of the project that should be cloned
+     * @return The new name for a cloned project, the name does not exist until now
+     */
+    private static String cloneProjectTitleWithoutNameConflict(String sourceProjectTitle) {
+        String newProjectTitle = sourceProjectTitle + "_copy";
+        List<String> existingProjectTitles = ProjectManager.getAllProjectTitles(false);
+        if (!existingProjectTitles.contains(newProjectTitle)) {
+            return newProjectTitle;
+        }
+        newProjectTitle += "_";
+        int cloneIndex = 1;
+        do {
+            cloneIndex++;
+        } while (existingProjectTitles.contains(newProjectTitle + cloneIndex));
+        return newProjectTitle + cloneIndex;
+    }
+
 }
