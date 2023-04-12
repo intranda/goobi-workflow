61a0a6803d94d1f5d345052f3303fa3a4cb8d8f4
Robert Sehr <robert.sehr@intranda.com>
2023-04-12 13:54:06 +0200

    added junit tests for change process rest api call refs #24517

--- Goobi/src/test/java/org/goobi/api/rest/ProcessServiceTest.java
+++ Goobi/src/test/java/org/goobi/api/rest/ProcessServiceTest.java
@@ -22,11 +22,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.core.Response;
 
 import org.easymock.EasyMock;
 import org.goobi.api.rest.model.RestProcessResource;
+import org.goobi.beans.Batch;
+import org.goobi.beans.Docket;
+import org.goobi.beans.Institution;
 import org.goobi.beans.Process;
+import org.goobi.beans.Project;
+import org.goobi.beans.Ruleset;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,10 +44,17 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import de.sub.goobi.AbstractTest;
 import de.sub.goobi.mock.MockProcess;
+import de.sub.goobi.persistence.managers.DocketManager;
+import de.sub.goobi.persistence.managers.MasterpieceManager;
 import de.sub.goobi.persistence.managers.ProcessManager;
+import de.sub.goobi.persistence.managers.ProjectManager;
+import de.sub.goobi.persistence.managers.PropertyManager;
+import de.sub.goobi.persistence.managers.RulesetManager;
+import de.sub.goobi.persistence.managers.TemplateManager;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(ProcessManager.class)
+@PrepareForTest({ ProcessManager.class, ProjectManager.class, RulesetManager.class, DocketManager.class, PropertyManager.class, TemplateManager.class,
+    MasterpieceManager.class })
 @PowerMockIgnore({ "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "org.w3c.*", "javax.management.*" })
 public class ProcessServiceTest extends AbstractTest {
 
@@ -55,10 +69,52 @@ public class ProcessServiceTest extends AbstractTest {
         resource.setId(1);
 
         Process process = MockProcess.createProcess();
+        process.setIstTemplate(true);
+        process.setSortHelperDocstructs(5);
+        process.setSortHelperImages(5);
+        process.setSortHelperMetadata(5);
+        process.setSortHelperStatus("050050000");
+
+        Project otherProject = new Project();
+        otherProject.setTitel("other");
+        Institution inst = new Institution();
+        inst.setShortName("name");
+        process.getProjekt().setInstitution(inst);
+        otherProject.setInstitution(inst);
+        Ruleset otherRuleset = new Ruleset();
+        otherRuleset.setTitel("otherRuleset");
+        Docket docket = new Docket();
+        docket.setName("docket");
+        process.setDocket(docket);
+        Docket otherDocket = new Docket();
+        otherDocket.setName("otherDocket");
+
+        Batch batch = new Batch();
+        batch.setBatchId(1);
+        batch.setBatchLabel("label");
+
         PowerMock.mockStatic(ProcessManager.class);
         EasyMock.expect(ProcessManager.getProcessById(1)).andReturn(process).anyTimes();
-
+        EasyMock.expect(ProcessManager.getBatchById(EasyMock.anyInt())).andReturn(batch).anyTimes();
+        EasyMock.expect(ProcessManager.countProcessTitle(EasyMock.anyString(), EasyMock.anyObject())).andReturn(0).anyTimes();
         ProcessManager.saveProcessInformation(EasyMock.anyObject());
+        ProcessManager.saveProcessInformation(EasyMock.anyObject());
+
+        PowerMock.mockStatic(ProjectManager.class);
+        EasyMock.expect(ProjectManager.getProjectByName(EasyMock.anyString())).andReturn(otherProject).anyTimes();
+
+        PowerMock.mockStatic(RulesetManager.class);
+        EasyMock.expect(RulesetManager.getRulesetByName(EasyMock.anyString())).andReturn(otherRuleset).anyTimes();
+
+        PowerMock.mockStatic(DocketManager.class);
+        EasyMock.expect(DocketManager.getDocketByName(EasyMock.anyString())).andReturn(otherDocket).anyTimes();
+
+        PowerMock.mockStatic(PropertyManager.class);
+        EasyMock.expect(PropertyManager.getProcessPropertiesForProcess(EasyMock.anyInt())).andReturn(new ArrayList<>()).anyTimes();
+        PowerMock.mockStatic(TemplateManager.class);
+        EasyMock.expect(TemplateManager.getTemplatesForProcess(EasyMock.anyInt())).andReturn(new ArrayList<>()).anyTimes();
+        PowerMock.mockStatic(MasterpieceManager.class);
+        EasyMock.expect(MasterpieceManager.getMasterpiecesForProcess(EasyMock.anyInt())).andReturn(new ArrayList<>()).anyTimes();
 
         EasyMock.expectLastCall();
         PowerMock.replayAll();
@@ -85,17 +141,54 @@ public class ProcessServiceTest extends AbstractTest {
     @Test
     public void testUpdateProcess() {
         service = new ProcessService();
+
+        resource.setId(0);
         Response response = service.updateProcess(resource);
         assertNotNull(response);
+        assertEquals(400, response.getStatus());
+
+        resource.setId(1);
+        response = service.updateProcess(resource);
         RestProcessResource res = (RestProcessResource) response.getEntity();
         assertEquals(1, res.getId());
         assertEquals("testprocess", res.getTitle());
+        assertEquals(5, res.getNumberOfDocstructs().intValue());
+        assertEquals(5, res.getNumberOfImages().intValue());
+        assertEquals(5, res.getNumberOfMetadata().intValue());
+        assertEquals("050050000", res.getStatus());
+        assertEquals("project", res.getProjectName());
+        assertEquals("ruleset.xml", res.getRulesetName());
+        assertEquals("docket", res.getDocketName());
+        assertNull(res.getBatchNumber());
+
+        resource.setNumberOfDocstructs(10);
+        resource.setNumberOfImages(10);
+        resource.setNumberOfMetadata(10);
+        resource.setStatus("100000000");
+        resource.setProjectName("other");
+        resource.setRulesetName("otherRuleset");
+        resource.setDocketName("otherDocket");
+        resource.setBatchNumber(1);
+
+        resource.setTitle("newTitle");
+
+        response = service.updateProcess(resource);
+        res = (RestProcessResource) response.getEntity();
+        assertEquals(10, res.getNumberOfDocstructs().intValue());
+        assertEquals(10, res.getNumberOfImages().intValue());
+        assertEquals(10, res.getNumberOfMetadata().intValue());
+        assertEquals("100000000", res.getStatus());
+        assertEquals("other", res.getProjectName());
+        assertEquals("otherRuleset", res.getRulesetName());
+        assertEquals("otherDocket", res.getDocketName());
+        assertEquals(1, res.getBatchNumber().intValue());
+        assertEquals("newTitle", res.getTitle());
     }
 
     @Test
     public void testCreateProcess() {
         service = new ProcessService();
-        Response response = service.createProcess( resource);
+        Response response = service.createProcess(resource);
         assertNull(response);
     }
 
