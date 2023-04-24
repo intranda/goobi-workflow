7b0a603ddfd25ec53bb8c294ac433f3c54a590b5
Robert Sehr <robert.sehr@intranda.com>
2023-04-24 08:31:11 +0200

    added missing license header

--- Goobi/src/main/java/org/goobi/api/rest/AuthenticationToken.java
+++ Goobi/src/main/java/org/goobi/api/rest/AuthenticationToken.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
+ * 
+ * Visit the websites for more information.
+ *          - https://goobi.io
+ *          - https://www.intranda.com
+ *          - https://github.com/intranda/goobi-workflow
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 package org.goobi.api.rest;
 
 import java.lang.reflect.InvocationTargetException;
@@ -6,6 +24,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.reflections.Reflections;
+
+import com.rometools.rome.io.impl.Base64;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -48,5 +68,9 @@ public class AuthenticationToken {
                 log.error(e);
             }
         }
+    }
+
+    public String getEncodedTokenName() {
+        return Base64.encode(tokenName);
     }
 }
