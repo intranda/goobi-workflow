11c20fbf11cf6033ce9f342d11544812ceeb2229
zehong <zehong.hu@intranda.com>
2023-05-24 13:17:21 +0200

    move licence declarations up to the first line

--- Goobi/src/main/java/de/sub/goobi/helper/ProcessTitleGenerator.java
+++ Goobi/src/main/java/de/sub/goobi/helper/ProcessTitleGenerator.java
@@ -1,5 +1,3 @@
-package de.sub.goobi.helper;
-
 /**
  * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
  * 
@@ -17,6 +15,8 @@ package de.sub.goobi.helper;
  * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59
  * Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
+
+package de.sub.goobi.helper;
 
 import java.util.ArrayList;
 import java.util.List;
