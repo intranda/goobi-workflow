4a1a3d73c3163be949d7c99a73f77570b2e7d151
Robert Sehr <robert.sehr@intranda.com>
2023-07-12 09:56:50 +0200

    prepared sql methods for new goobiscript template refs #24801

---
+++ Goobi/src/main/java/de/sub/goobi/persistence/managers/GoobiScriptTemplateMysqlHelper.java
@@ -0,0 +1,57 @@
+/**
+ * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
+ * 
+ * Visit the websites for more information.
+ *          - https://goobi.io
+ *          - https://www.intranda.com
+ *          - https://github.com/intranda/goobi-workflow
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package de.sub.goobi.persistence.managers;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.goobi.goobiScript.GoobiScriptTemplate;
+
+public class GoobiScriptTemplateMysqlHelper {
+
+    public static int getGoobiScriptTemplateCount(String filter) throws SQLException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public static GoobiScriptTemplate getGoobiScriptTemplateById(int id) throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void saveGoobiScriptTemplate(GoobiScriptTemplate template) throws SQLException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public static void deleteGoobiScriptTemplate(GoobiScriptTemplate template) throws SQLException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public static List<GoobiScriptTemplate> getGoobiScriptTemplates(String order, String filter, Integer start, Integer count) throws SQLException{
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static List<GoobiScriptTemplate> getAllGoobiScriptTemplates() throws SQLException{
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
