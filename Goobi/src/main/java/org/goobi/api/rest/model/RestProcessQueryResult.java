37f883542eee4b9cf18d4810968a83a7e1ef0293
zehong <zehong.hu@intranda.com>
2023-09-22 13:02:36 +0200

    remove @Setter from RestProcessQueryResult

--- Goobi/src/main/java/org/goobi/api/rest/model/RestProcessQueryResult.java
+++ Goobi/src/main/java/org/goobi/api/rest/model/RestProcessQueryResult.java
@@ -28,13 +28,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @XmlRootElement(name = "query")
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
-@Setter
 public class RestProcessQueryResult {
     private int results;
     private Integer[] ids;
