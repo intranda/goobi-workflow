86955741c5038eaa1b4da37de1b2414e040c57a2
Robert Sehr <robert.sehr@intranda.com>
2023-10-04 11:56:00 +0200

    import marcxml records refs #25187

--- Goobi/src/main/java/org/goobi/api/rest/MetadataService.java
+++ Goobi/src/main/java/org/goobi/api/rest/MetadataService.java
@@ -9,10 +9,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.commons.lang.StringUtils;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.goobi.beans.Process;
 import org.goobi.beans.Project;
@@ -50,7 +52,6 @@ public class MetadataService implements IRestAuthentication {
         return null;
     }
 
-
     @POST
     @Consumes("*/*")
     public void upload(InputStream stream) throws IOException {
@@ -73,12 +74,12 @@ public class MetadataService implements IRestAuthentication {
     @Path("/{processid}/marc")
     @POST
     @Operation(summary = "Replace existing metadata with the content of the marc file",
-    description = "Replace existing metadata with the content of the marc file")
+            description = "Replace existing metadata with the content of the marc file")
     @ApiResponse(responseCode = "200", description = "OK")
     @ApiResponse(responseCode = "400", description = "Bad request")
     @ApiResponse(responseCode = "404", description = "Process not found")
     @ApiResponse(responseCode = "500", description = "Internal error")
-    public Response uploadMarcRecord(@PathParam("processid") Integer processid,    @FormDataParam("file") InputStream inputStream) {
+    public Response uploadMarcRecord(@PathParam("processid") Integer processid, @FormDataParam("file") InputStream inputStream) {
 
         Process process = ProcessManager.getProcessById(processid);
 
@@ -108,7 +109,6 @@ public class MetadataService implements IRestAuthentication {
         DocumentBuilder builder = dbf.newDocumentBuilder();
         Document doc = builder.parse(inputStream);
         MarcFileformat fileformat = new MarcFileformat(prefs);
-        // TODO add collection element, if needed
 
         fileformat.read(doc.getDocumentElement());
 
@@ -124,13 +124,16 @@ public class MetadataService implements IRestAuthentication {
 
     @HarvesterGoobiImport(description = "Import MARC-XML Records")
     @POST
-    @Path("/{projectName}/{templateId}/marc")
-    @Consumes("*/*")
-    //    public Response uploadFile(@PathParam("processId") int processId, @PathParam("folder") final String folder,
-    //            @FormDataParam("file") InputStream fileInputStream, @FormDataParam("file") FormDataContentDisposition fileMetaData,
-    //            @FormDataParam("filename") String filename)
-
-    public Response createProcessWithMarcRecord(@PathParam("projectName") String projectName, @PathParam("templateId") Integer templateId,
+    @Path("/{projectName}/{templateName}/{processTitle}/marc")
+    @Operation(summary = "Create a process with given marc file",
+            description = "Create a new process, get metadata from content of the marc file ")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "404", description = "Project not found or process template not found.")
+    @ApiResponse(responseCode = "409", description = "The process title is already used.")
+    @ApiResponse(responseCode = "500", description = "Internal error")
+    public Response createProcessWithMarcRecord(@PathParam("projectName") String projectName, @PathParam("templateName") String templateName,
+            @PathParam("processTitle") String processTitle,
             InputStream inputStream) {
         Project project = null;
         try {
@@ -139,22 +142,29 @@ public class MetadataService implements IRestAuthentication {
             log.error(e);
             return Response.status(500).entity("Cannot read project").build();
         }
+
         if (project == null) {
             return Response.status(404).entity("Project not found").build();
         }
 
-        Process template = ProcessManager.getProcessById(templateId);
+        Process template = ProcessManager.getProcessByExactTitle(templateName);
         if (template == null) {
             return Response.status(404).entity("Process template not found").build();
         }
+        // generate temporary process title if missing
+        if (StringUtils.isBlank(processTitle) || "-".equals(processTitle)) {
+            processTitle = String.valueOf(System.currentTimeMillis());
+        }
 
-        // create dummy process
-        String tempTitle = String.valueOf(System.currentTimeMillis());
-        Process process = ProcessService.prepareProcess(tempTitle, template);
+        // check for duplicates
+        Process other = ProcessManager.getProcessByExactTitle(processTitle);
+        if (other != null) {
+            return Response.status(Status.CONFLICT).entity("The process title is already used.").build();
+        }
+
+        Process process = ProcessService.prepareProcess(processTitle, template);
         process.setProjekt(project);
         Prefs prefs = process.getRegelsatz().getPreferences();
-
-        // TODO process title
 
         try {
             // save process to create id and directories
@@ -162,6 +172,7 @@ public class MetadataService implements IRestAuthentication {
             // save metadata file
             Fileformat ff = readMarcMetadataFile(inputStream, prefs);
             process.writeMetadataFile(ff);
+            log.debug("Generated process {} using marc upload", processTitle);
         } catch (DAOException | UGHException | ParserConfigurationException | SAXException | IOException | SwapException e) {
             log.error(e);
         }
