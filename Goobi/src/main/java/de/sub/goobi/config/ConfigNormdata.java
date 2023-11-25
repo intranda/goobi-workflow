2acdaf473ceef966eecaabc4e497c5eb69adf789
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2023-11-25 19:47:33 +0100

    added some javadoc

--- Goobi/src/main/java/de/sub/goobi/config/ConfigNormdata.java
+++ Goobi/src/main/java/de/sub/goobi/config/ConfigNormdata.java
@@ -1,12 +1,19 @@
 package de.sub.goobi.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.goobi.api.display.helper.NormDatabase;
+
+import lombok.extern.log4j.Log4j2;
+
 /**
  * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
  * 
- * Visit the websites for more information.
- *          - https://goobi.io
- *          - https://www.intranda.com
- *          - https://github.com/intranda/goobi-workflow
+ * Visit the websites for more information. - https://goobi.io - https://www.intranda.com - https://github.com/intranda/goobi-workflow
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option) any later version.
@@ -18,22 +25,18 @@ package de.sub.goobi.config;
  * Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
  */
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.XMLConfiguration;
-import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
-import org.goobi.api.display.helper.NormDatabase;
-
-import lombok.extern.log4j.Log4j2;
-
 @Log4j2
 public class ConfigNormdata {
 
     private ConfigNormdata() {
     }
 
+    /**
+     * Retrieves a list of configured normdatabases from the 'goobi_normdata.xml' configuration file. If the configuration file is not present or no
+     * normdatabases are configured, a default normdatabase is added.
+     * 
+     * @return A list of NormDatabase objects representing the configured normdatabases.
+     */
     public static List<NormDatabase> getConfiguredNormdatabases() {
         List<NormDatabase> answer = new ArrayList<>();
         XMLConfiguration config = getNormdataConfiguration();
@@ -50,6 +53,11 @@ public class ConfigNormdata {
         return answer;
     }
 
+    /**
+     * Loads and returns the XML configuration for normdata from the 'goobi_normdata.xml' file.
+     * 
+     * @return The XMLConfiguration object representing the normdata configuration.
+     */
     private static XMLConfiguration getNormdataConfiguration() {
         String configurationFile = ConfigurationHelper.getInstance().getConfigurationFolder() + "goobi_normdata.xml";
         XMLConfiguration config = new XMLConfiguration();
