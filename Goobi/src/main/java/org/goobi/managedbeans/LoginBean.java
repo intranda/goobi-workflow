191bd61eb97d836beae1f51c56bc3804e08d978a
djo011 <djo011@uib.no>
2024-02-23 13:17:32 +0100

    rewrite method openIDLogin to support different flow type

--- Goobi/src/main/java/org/goobi/managedbeans/LoginBean.java
+++ Goobi/src/main/java/org/goobi/managedbeans/LoginBean.java
@@ -457,27 +457,17 @@ public class LoginBean implements Serializable {
     public void openIDLogin() {
         ConfigurationHelper config = ConfigurationHelper.getInstance();
         ExternalContext ec = FacesContextHelper.getCurrentFacesContext().getExternalContext();
-        byte[] secureBytes = new byte[64];
-        new SecureRandomNumberGenerator().getSecureRandom().nextBytes(secureBytes);
-        String nonce = Base64.getUrlEncoder().encodeToString(secureBytes);
-        HttpSession session = (HttpSession) ec.getSession(false);
-        session.setAttribute("openIDNonce", nonce);
-        String applicationPath = ec.getApplicationContextPath();
-        HttpServletRequest hreq = (HttpServletRequest) ec.getRequest();
-        try {
-            URIBuilder builder = new URIBuilder(config.getOIDCAuthEndpoint());
-            builder.addParameter("client_id", config.getOIDCClientID());
-            builder.addParameter("response_type", "id_token");
-            builder.addParameter("redirect_uri",
-                    hreq.getScheme() + "://" + hreq.getServerName() + ":" + hreq.getServerPort() + applicationPath + "/api/login/openid");
-            builder.addParameter("response_mode", "form_post");
-            builder.addParameter("scope", "openid");
-            builder.addParameter("nonce", nonce);
-
-            ec.redirect(builder.build().toString());
-        } catch (URISyntaxException | IOException e) {
-            // TODO Auto-generated catch block
-            log.error(e);
+        
+        String flowType = config.getOIDCFlowType();
+        
+        if (flowType == "ImplicitFlow") {
+        	openIDIFLogin();        	
+        }
+        if (flowType == "AuthorizationCodeFlow") {
+        	openIDACFLogin();        	
+        }
+        else { // Default case, when FlowType is not set in config file.
+        	openIDIFLogin();        	
         }
     }
 
