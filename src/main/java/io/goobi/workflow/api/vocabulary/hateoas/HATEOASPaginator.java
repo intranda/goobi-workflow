975e5744357d9e3d6e36fe68c393e221d701dcec
Dominick Leppich <dominick.leppich@intranda.com>
2024-07-19 15:26:25 +0200

    refactor: adapt to new all-in-one search and list endpoint and fix search clearing bug

--- src/main/java/io/goobi/workflow/api/vocabulary/hateoas/HATEOASPaginator.java
+++ src/main/java/io/goobi/workflow/api/vocabulary/hateoas/HATEOASPaginator.java
@@ -22,7 +22,6 @@ public class HATEOASPaginator<T extends Identifiable, PageT extends BasePageResu
     public static final String NAVIGATE_NEXT = "next";
     public static final String NAVIGATE_FIRST = "first";
     public static final String NAVIGATE_LAST = "last";
-    public static final String SEARCH_ENDPOINT = "/search";
 
     private final Client client = ClientBuilder.newClient();
     private final Optional<Runnable> resetCallback;
@@ -100,16 +99,12 @@ public class HATEOASPaginator<T extends Identifiable, PageT extends BasePageResu
         pageSize.ifPresent(value -> parameters.put("size", String.valueOf(value)));
         pageNumber.ifPresent(value -> parameters.put("page", String.valueOf(value)));
         sortField.ifPresent(s -> parameters.put("sort", s));
-        searchParameter.ifPresent(s -> parameters.put("query", s));
+        searchParameter.ifPresent(s -> parameters.put("search", s));
+        if (searchParameter.isEmpty()) {
+            parameters.remove("search");
+        }
         if (questionMarkIndex < 0) {
             url += "?";
-            questionMarkIndex = url.length() - 1;
-        }
-        if (searchParameter.isPresent() && !url.substring(0, questionMarkIndex).endsWith(SEARCH_ENDPOINT)) {
-            url = url.substring(0, questionMarkIndex) + SEARCH_ENDPOINT + "?";
-            questionMarkIndex = url.length() - 1;
-        } else if (searchParameter.isEmpty() && url.substring(0, questionMarkIndex).endsWith(SEARCH_ENDPOINT)) {
-            url = url.substring(0, questionMarkIndex).replace(SEARCH_ENDPOINT, "") + "?";
             questionMarkIndex = url.length() - 1;
         }
         url = url.substring(0, questionMarkIndex + 1) + parameters.entrySet().stream()
