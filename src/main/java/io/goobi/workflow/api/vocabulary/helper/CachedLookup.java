a735a4a5f06446cc225a3a000ab048352285047d
Dominick Leppich <dominick.leppich@intranda.com>
2024-07-29 08:11:36 +0200

    task: implement aging cache, reload after some time

--- src/main/java/io/goobi/workflow/api/vocabulary/helper/CachedLookup.java
+++ src/main/java/io/goobi/workflow/api/vocabulary/helper/CachedLookup.java
@@ -1,11 +1,27 @@
 package io.goobi.workflow.api.vocabulary.helper;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 public class CachedLookup<T> {
-    private Map<Long, T> cache = new HashMap<>();
+    private static final long MAX_DATA_AGE_IN_MS = 5000L;
+
+    @Data
+    @AllArgsConstructor
+    private class AgingData {
+        private final long timestamp;
+        private final T data;
+
+        public long getAge() {
+            return System.currentTimeMillis() - getTimestamp();
+        }
+    }
+
+    private Map<Long, AgingData> cache = new HashMap<>();
     private Function<Long, T> lookupFunction;
 
     public CachedLookup(Function<Long, T> lookupFunction) {
@@ -14,16 +30,29 @@ public class CachedLookup<T> {
 
     public synchronized T getCached(Long id) {
         if (!cache.containsKey(id)) {
-            cache.put(id, this.lookupFunction.apply(id));
+            return insert(id);
         }
-        return cache.get(id);
+        AgingData agingData = cache.get(id);
+        if (agingData.getAge() > MAX_DATA_AGE_IN_MS) {
+            return insert(id);
+        }
+        return agingData.getData();
     }
 
-    public synchronized void update(Long id, T item) {
-        this.cache.put(id, item);
+    public synchronized T update(Long id, T item) {
+        return insert(id, item);
     }
 
     public synchronized void invalidate(Long id) {
         this.cache.remove(id);
+    }
+
+    private T insert(Long id) {
+        return insert(id, this.lookupFunction.apply(id));
+    }
+
+    private T insert(Long id, T data) {
+        cache.put(id, new AgingData(System.currentTimeMillis(), data));
+        return data;
     }
 }
