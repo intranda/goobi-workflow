dd14ec38ab9a2dff660ee5d35b04c6b20632586d
Dominick Leppich <dominick.leppich@intranda.com>
2024-12-16 11:18:58 +0100

    wip: rework

--- src/main/java/org/goobi/production/plugin/barcode/BarcodeScannerPlugin.java
+++ src/main/java/org/goobi/production/plugin/barcode/BarcodeScannerPlugin.java
@@ -1,10 +1,13 @@
 package org.goobi.production.plugin.barcode;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.goobi.production.plugin.barcode.config.BarcodeFormat;
+import org.goobi.production.plugin.barcode.config.BarcodeScannerPluginConfiguration;
 import org.goobi.production.plugin.interfaces.IFooterPlugin;
 
+import java.io.File;
 import java.util.Collections;
 import java.util.List;
 
@@ -32,17 +35,33 @@ public class BarcodeScannerPlugin implements IFooterPlugin {
     }
 
     private String code;
-    private List<BarcodeFormat> barcodeFormats = Collections.emptyList();
+    private BarcodeScannerPluginConfiguration config;
     private List<BarcodeFormat> activeFormats = Collections.emptyList();
+
+    public static void main(String[] args) throws Exception {
+        BarcodeScannerPlugin plugin = new BarcodeScannerPlugin();
+        plugin.initialize();
+        plugin.config.getBarcode().get(0).activate("propertyset_Standort_54");
+    }
 
     @Override
     public void initialize() throws Exception {
-        barcodeFormats = List.of(new BarcodeFormat("place_(.*)", """
----
-action: propertySet
-name: {{1}}
-value: {{?}}
-"""));
+        XmlMapper mapper = new XmlMapper();
+        this.config = mapper.readValue(new File("/opt/digiverso/goobi/config/plugin_intranda_footer_barcodeScanner.xml"), BarcodeScannerPluginConfiguration.class);
+//        this.config = new BarcodeScannerPluginConfiguration();
+//        this.config.setBarcode(List.of(new BarcodeFormat("place_(.*)_(\\d+)", """
+//---
+//action: propertySet
+//name: {{1}}
+//value: {{2}}
+//"""),
+//                new BarcodeFormat("restauration_(.*)_(\\d+)", """
+//---
+//action: propertySet
+//name: {{1}}
+//value: {{2}}
+//""")));
+//        mapper.writeValue(System.err, this.config);
     }
 
     public void scan() {
@@ -51,7 +70,7 @@ value: {{?}}
         List<BarcodeFormat> applicable = findMatchingBarcodeFormats(code);
 
         if (!applicable.isEmpty()) {
-            applicable.forEach(bf -> bf.process(code));
+            applicable.forEach(bf -> bf.activate(code));
             activeFormats = applicable;
         } else {
             if (!activeFormats.isEmpty()) {
@@ -60,19 +79,10 @@ value: {{?}}
                 log.warn("No active barcode formats found");
             }
         }
-
-//        PropertyManager.getProcessPropertiesForProcess(Integer.parseInt(code)).stream()
-//                .filter(p -> p.getTitel().equals("Standort"))
-//                .findFirst()
-//                .ifPresent(p -> {
-//                    p.setWert("54");
-//                    PropertyManager.saveProcessProperty(p);
-//                    Helper.addMessageToProcessJournal(Integer.parseInt(code), LogType.DEBUG, "Process property \"Standort\" was set by barcode to new value \"54\".");
-//                });
     }
 
     private List<BarcodeFormat> findMatchingBarcodeFormats(String code) {
-        return barcodeFormats.stream()
+        return this.config.getBarcode().stream()
                 .filter(bf -> bf.patternMatches(code))
                 .toList();
     }
