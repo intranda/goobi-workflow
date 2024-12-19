1c81d32975ab838677f3e7f9753e8640f29ced12
Dominick Leppich <dominick.leppich@intranda.com>
2024-12-19 10:17:35 +0100

    wip: try dynamic loading

--- src/main/java/org/goobi/production/plugin/interfaces/IFooterPlugin.java
+++ src/main/java/org/goobi/production/plugin/interfaces/IFooterPlugin.java
@@ -2,6 +2,15 @@ package org.goobi.production.plugin.interfaces;
 
 import org.goobi.production.enums.PluginType;
 
+import javax.faces.application.Application;
+import javax.faces.application.Resource;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIPanel;
+import javax.faces.component.html.HtmlPanelGroup;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.FaceletContext;
+import java.io.IOException;
+
 public interface IFooterPlugin extends IPlugin {
     default String getId() {
         return getTitle().replaceAll("\\s+", "");
@@ -18,7 +27,7 @@ public interface IFooterPlugin extends IPlugin {
 
     void execute() throws Exception;
 
-    default String getModal() {
+    default String getModalPath() {
         return null;
     }
 }
