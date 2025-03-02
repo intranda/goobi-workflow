3a420eb5f06c335cc4cae63cf5e76948fa95f969
Simon Sendler <simon.sendler@intranda.com>
2025-03-02 10:07:39 +0100

    refactor: rebuild metadata editor keyboard shortcuts

--- src/main/webapp/uii/template/js/dev/goobiWorkflowJS.navigation.js
+++ src/main/webapp/uii/template/js/dev/goobiWorkflowJS.navigation.js
@@ -1,9 +1,9 @@
 var goobiWorkflowJS = ( function( goobiWorkflow ) {
     'use strict';
-    
+
     var _debug = false;
     var _defaults = {};
-    
+
     goobiWorkflow.navigation = {
         /**
          * @description Method to initialize the buttons.
@@ -13,13 +13,14 @@ var goobiWorkflowJS = ( function( goobiWorkflow ) {
             if ( _debug ) {
                 console.log( 'Initializing: goobiWorkflowJS.navigation.init' );
             }
-            
+
             $.extend( true, _defaults, config );
-            
+
+            console.log('init navigation');
             _setImageNavigationButtonEvents();
         }
     };
-    
+
     function _checkNavigationBlocked() {
     	//The objects in window.imageNavBlockers look like this:
     	//{check: function, confirm: "string"}
@@ -37,180 +38,108 @@ var goobiWorkflowJS = ( function( goobiWorkflow ) {
     	}
     	return blocked;
     }
-    
+
+    const findImageOrThumbButton = ( imageSelector, thumbSelector ) => {
+        if (document.querySelector( '#thumbnailsNavigation')) {
+            clickButton( thumbSelector );
+        }
+        if (document.querySelector( '#imageNavigation')) {
+            clickButton( imageSelector );
+        }
+    }
+
+    const clickButton = ( selector ) => {
+        const button = document.querySelector( selector );
+        if ( button != null ) {
+            button.click();
+        }
+    };
+
+    const nextImage = () => {
+        if(_checkNavigationBlocked()) {
+            return;
+        }
+        findImageOrThumbButton( '#nextImage', '#thumbNext' );
+    };
+
+    const prevImage = () => {
+        if(_checkNavigationBlocked()) {
+            return;
+        }
+        findImageOrThumbButton( '#prevImage', '#thumbPrev' );
+    };
+
+    const imageNext20 = () => {
+        if(_checkNavigationBlocked()) {
+            return;
+        }
+        clickButton( '#imageNext20' );
+    };
+
+    const imageBack20 = () => {
+        if(_checkNavigationBlocked()) {
+            return;
+        }
+        clickButton( '#imageBack20' );
+    };
+
+    const imageFirst = () => {
+        if(_checkNavigationBlocked()) {
+            return;
+        }
+        findImageOrThumbButton( '#imageFirst', '#thumbFirst' );
+    };
+
+    const imageLast = () => {
+        if(_checkNavigationBlocked()) {
+            return;
+        }
+        findImageOrThumbButton( '#imageLast', '#thumbLast' );
+    };
+
+    const save = () => {
+        clickButton( '[id$="saveMetsFileButtonAjax"]' );
+    };
+
+    const validate = () => {
+        clickButton( '[id$="startValidationButton"]' );
+    };
+
+    const selectImage = () => {
+        const hiddenImageNo = document.querySelector( '[id$="hiddenImageNo"]' ).value;
+        if ( hiddenImageNo == null ) {
+            hiddenImageNo = document.querySelector( '[id$="hiddenImageNoThumb"]' ).value;
+        }
+        const checkbox = document.querySelector('[id$="pagesList:' + hiddenImageNo + ':checkbox"]');
+        if ( checkbox.checked ) {
+            checkbox.checked = false;
+        }
+        else {
+            checkbox.checked = true;
+        }
+        checkbox.focus();
+        checkbox.blur();
+        $( document ).off( 'keyup', null, _defaults.navigationShortcut + '+space' );
+    };
+
     function _setImageNavigationButtonEvents() {
         if ( _debug ) {
             console.log( 'EXECUTE: _setImageNavigationButtonEvents' );
         }
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+right', function(e) {
-        	if(_checkNavigationBlocked()) {
-        		return;
-        	}
-            var myButton = document.getElementById( "nextImage" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+left', function() {
-        	if(_checkNavigationBlocked()) {
-        		return;
-        	}
-            var myButton = document.getElementById( "prevImage" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+up', function() {
-        	if(_checkNavigationBlocked()) {
-        		return;
-        	}
-            var myButton = document.getElementById( "imageNext20" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+down', function() {
-        	if(_checkNavigationBlocked()) {
-        		return;
-        	}
-            var myButton = document.getElementById( "imageBack20" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+home', function() {
-        	if(_checkNavigationBlocked()) {
-        		return;
-        	}
-            var myButton = document.getElementById( "imageFirst" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+end', function() {
-        	if(_checkNavigationBlocked()) {
-        		return;
-        	}
-            var myButton = document.getElementById( "imageLast" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+return', function() {
-            var myButton = document.getElementById( "meMenuActionsForm:saveMetsFileButtonAjax" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+v', function() {
-            var myButton = document.getElementById( "meMenuActionsForm:startValidationButton" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( document ).bind( 'keyup', _defaults.navigationShortcut + '+space', function() {
-            var hiddenImageNo = $( "#hiddenImageNo" ).val();
-            if ( hiddenImageNo == null ) {
-                hiddenImageNo = $( "#hiddenImageNoThumb" ).val();
-            }
-            var checkbox = document.getElementById( "pagesList:" + hiddenImageNo + ":checkbox" );
-            
-            if ( checkbox.checked ) {
-                checkbox.checked = false;
-            }
-            else {
-                checkbox.checked = true;
-            }
-            checkbox.focus();
-            checkbox.blur();
-            
-        } )
 
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+right', function() {
-            var myButton = document.getElementById( "nextImage" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+left', function() {
-            var myButton = document.getElementById( "prevImage" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+up', function() {
-            var myButton = document.getElementById( "imageNext20" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+down', function() {
-            var myButton = document.getElementById( "imageBack20" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+home', function() {
-            var myButton = document.getElementById( "imageFirst" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+end', function() {
-            var myButton = document.getElementById( "imageLast" );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+return', function() {
-            
-            var myButton = document.getElementById( "meMenuActionsForm:saveMetsFileButtonAjax" );
-            console.log( myButton );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+v', function() {
-            
-            var myButton = document.getElementById( "meMenuActionsForm:startValidationButton" );
-            console.log( myButton );
-            if ( myButton != null ) {
-                myButton.click();
-            }
-        } );
-        
-        $( 'input:checkbox' ).bind( 'keyup', _defaults.navigationShortcut + '+space', function() {
-            var hiddenImageNo = $( "#hiddenImageNo" ).val();
-            for ( i = 0; i < document.getElementsByName( "myCheckboxes" ).length; i++ ) {
-                if ( i == hiddenImageNo ) {
-                    var checkbox = document.getElementsByName( "myCheckboxes" )[ i ];
-                    if ( checkbox.checked ) {
-                        checkbox.checked = false;
-                    }
-                    else {
-                        checkbox.checked = true;
-                    }
-                }
-            }
-        } );
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+right', nextImage );
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+left', prevImage );
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+up', imageNext20 );
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+down', imageBack20 );
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+home', imageFirst );
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+end', imageLast );
+
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+return', save );
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+v', validate );
+        $( document ).on( 'keyup', null, _defaults.navigationShortcut + '+space', selectImage );
+
     }
-    
+
     return goobiWorkflow;
-    
+
 } )( goobiWorkflowJS || {}, jQuery );
