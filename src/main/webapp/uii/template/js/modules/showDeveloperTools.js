30f890adbdc5e8a2a6e1da35991762413b5bcbf9
Simon Sendler <simon.sendler@intranda.com>
2025-06-06 11:04:00 +0200

    refactor: refactor dev button toggle JS

--- src/main/webapp/uii/template/js/modules/showDeveloperTools.js
+++ src/main/webapp/uii/template/js/modules/showDeveloperTools.js
@@ -1,22 +1,20 @@
 export const initShowDevelopertools = () => {
     const elements = document.querySelectorAll('[data-developer-tool');
     document.addEventListener('keydown', (event) => {
-        document.addEventListener('keydown', (e) => {
-            if (event.key === 'Alt' || event.key === 'Control') {
-                document.addEventListener('keydown', (event) => {
-                    if (event.key === 'Alt' || event.key === 'Control') {
-                        document.addEventListener('keyup', (e) => {
-                            if (e.key === 'd') {
-                                e.preventDefault();
-                                e.stopPropagation();
-                                elements.forEach((element) => {
-                                    element.classList.toggle('d-none');
-                                });
-                            }
-                        }, { once: true });
-                    }
-                });
-            };
-        });
+        if (event.key === 'Alt' || event.key === 'Control') {
+            document.addEventListener('keydown', (event) => {
+                if (event.key === 'Alt' || event.key === 'Control') {
+                    document.addEventListener('keyup', (e) => {
+                        if (e.key === 'd') {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            elements.forEach((element) => {
+                                element.classList.toggle('d-none');
+                            });
+                        }
+                    }, { once: true });
+                }
+            });
+        };
     }, { once: true });
 };
