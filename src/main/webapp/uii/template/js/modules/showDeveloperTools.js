f6e929ffc323b1c60243790b21c8e50d6da20ad9
Simon Sendler <simon.sendler@intranda.com>
2025-06-06 11:30:39 +0200

    refactor: change developer tool toogle method form key combination to magic pixel

--- src/main/webapp/uii/template/js/modules/showDeveloperTools.js
+++ src/main/webapp/uii/template/js/modules/showDeveloperTools.js
@@ -1,20 +1,14 @@
 export const initShowDevelopertools = () => {
+    const toggle = document.querySelector('[data-developer-tool-toggle]');
     const elements = document.querySelectorAll('[data-developer-tool');
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Alt' || event.key === 'Control') {
-            document.addEventListener('keydown', (event) => {
-                if (event.key === 'Alt' || event.key === 'Control') {
-                    document.addEventListener('keyup', (e) => {
-                        if (e.key === 'd') {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            elements.forEach((element) => {
-                                element.classList.toggle('d-none');
-                            });
-                        }
-                    }, { once: true });
-                }
+    if (toggle && elements.length > 0) {
+        toggle.addEventListener('click', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            console.debug('Toggling developer tools visibility');
+            elements.forEach((element) => {
+                element.classList.toggle('d-none');
             });
-        };
-    }, { once: true });
+        });
+    };
 };
