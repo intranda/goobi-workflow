cf8ca1b965826f28e3b6da70c8d1bc5d85a93316
Robert Sehr <robert.sehr@intranda.com>
2025-11-12 08:18:03 +0100

    junit tests for section, fixed NPE

--- src/main/java/org/goobi/beans/Section.java
+++ src/main/java/org/goobi/beans/Section.java
@@ -1,21 +1,40 @@
-package org.goobi.beans;
+/**
+ * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
+ *
+ * Visit the websites for more information.
+ *          - https://goobi.io
+ *          - https://www.intranda.com
+ *          - https://github.com/intranda/goobi-workflow
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
-import jakarta.enterprise.context.SessionScoped;
-import jakarta.inject.Named;
+package org.goobi.beans;
 
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.deltaspike.core.api.scope.WindowScoped;
+
+import jakarta.inject.Named;
+
 @Named
-@SessionScoped
+@WindowScoped
 public class Section implements Serializable {
     private static final long serialVersionUID = 650242038L;
 
     private Map<String, Boolean> sections = new HashMap<>();
 
     public Boolean isCollapsed(String sectionId) {
-        return sections.get(sectionId);
+        return isCollapsed(sectionId, false);
     }
 
     public boolean isCollapsed(String sectionId, boolean defaultValue) {
