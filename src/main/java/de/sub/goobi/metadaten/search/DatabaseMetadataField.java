4e216ee7c80e55ff5468e4ddf97fe80d42252749
Robert Sehr <robert.sehr@intranda.com>
2026-04-29 11:06:24 +0200

    added missing file

---
+++ src/main/java/de/sub/goobi/metadaten/search/DatabaseMetadataField.java
@@ -0,0 +1,42 @@
+/**
+ * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
+ * 
+ * Visit the websites for more information.
+ *          - https://goobi.io
+ *          - https://www.intranda.com
+ *          - https://github.com/intranda/goobi-workflow
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package de.sub.goobi.metadaten.search;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DatabaseMetadataField {
+
+    // internal metadata name
+    private String metadataName;
+    // actual value
+    private String metadataValue;
+    // name of the authority database
+    private String authorityName;
+    // uri to the authority database
+    private String authorityUri;
+    // authority value
+    private String authorityValue;
+}
