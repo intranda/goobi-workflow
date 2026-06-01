bc8f02f44dd2499e272e625ddd3da70a9b336b16
Lars Geiger <lars.geiger@intranda.com>
2026-06-01 09:37:10 +0200

    Configurable webapp path via env vars

--- install/docker/run.sh
+++ install/docker/run.sh
@@ -8,6 +8,7 @@ groupmod -o -g "${WORKFLOW_GID}" user
 usermod -o -u "${WORKFLOW_UID}" user
 
 [ -z "$CONFIGSOURCE" ] && CONFIGSOURCE="default"
+[ -z "${WORKFLOW_BASE_PATH+x}" ] && WORKFLOW_BASE_PATH="/workflow"
 
 set -u
 
@@ -17,10 +18,31 @@ mkdir -p /opt/digiverso/goobi/plugins/{administration,command,dashboard,export,g
 echo "Setting properties from environment variables"
 /usr/bin/python3 /config.py
 
-echo "Setting database configuration from environment..."
-envsubst "\$DB_HOST \$DB_PORT \$DB_NAME \$DB_USER \$DB_PASSWORD" </usr/local/tomcat/conf/workflow.xml.template > /usr/local/tomcat/conf/Catalina/localhost/workflow.xml
-
 set +u
+
+# /path/to/application -> path#to#application  (Tomcat nested-context convention)
+# / (root)             -> ROOT
+if [[ "$WORKFLOW_BASE_PATH" == "/" ]]; then
+  WEBAPP_NAME="ROOT"
+  WORKFLOW_BASE_PATH=""
+elif ! [[ "$WORKFLOW_BASE_PATH" =~ ^(/[a-zA-Z0-9_-]+)+$ ]]; then
+  echo "WORKFLOW_BASE_PATH invalid: 'WORKFLOW_BASE_PATH'"
+  exit 1
+else
+  WEBAPP_NAME="${WORKFLOW_BASE_PATH#/}"
+  WEBAPP_NAME="${WEBAPP_NAME//\//#}"
+  WORKFLOW_BASE_PATH="${WORKFLOW_BASE_PATH#/}"
+  # redirect to application path
+  rm -rf "${CATALINA_HOME}/webapps/ROOT" && mkdir "${CATALINA_HOME}/webapps/ROOT" && echo "<% response.sendRedirect(\"/${WORKFLOW_BASE_PATH}/\"); %>" > "${CATALINA_HOME}/webapps/ROOT/index.jsp"
+fi
+WEBAPP_DIR="${CATALINA_HOME}/webapps/${WEBAPP_NAME}"
+
+if [[ "$WEBAPP_NAME" != "workflow" && -d "${CATALINA_HOME}/webapps/workflow" && ! -d "$WEBAPP_DIR" ]]; then
+    mv "${CATALINA_HOME}/webapps/workflow" "$WEBAPP_DIR"
+fi
+
+echo "Setting database configuration from environment..."
+envsubst "\$DB_HOST \$DB_PORT \$DB_NAME \$DB_USER \$DB_PASSWORD" < "${CATALINA_HOME}/conf/workflow.xml.template" > "${CATALINA_HOME}/conf/Catalina/localhost/${WEBAPP_NAME}.xml"
 
 export MYSQL_PWD="${DB_PASSWORD}"
 
@@ -61,8 +83,8 @@ if [ -n "${WORKING_STORAGE:-}" ]
 then
   CATALINA_TMPDIR="${WORKING_STORAGE}/workflow/jvmtemp"
   mkdir -p "${CATALINA_TMPDIR}"
-  echo >> /usr/local/tomcat/bin/setenv.sh
-  echo "CATALINA_TMPDIR=${CATALINA_TMPDIR}" >> /usr/local/tomcat/bin/setenv.sh
+  echo >> "${CATALINA_HOME}/bin/setenv.sh"
+  echo "CATALINA_TMPDIR=${CATALINA_TMPDIR}" >> "${CATALINA_HOME}/bin/setenv.sh"
 fi
 
 case $CONFIGSOURCE in
